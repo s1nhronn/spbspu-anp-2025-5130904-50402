a3bcadbb80adb4bd3e2dea2d03c29ac9670edf4b
Alastor20 <nik_d.007@mail.ru>
2025-11-02 19:25:02 +0300

    input parameters handaling

--- dirko.nikita/P3/main.cpp
+++ dirko.nikita/P3/main.cpp
@@ -1,4 +1,28 @@
-int main(int argc, char const *argv[])
+#include <iostream>
+#include <cstring>
+
+int main(int argc, char const **argv)
 {
-  
+  int mode = 0;
+  if (argc < 4)
+  {
+    std::cerr << "Not enough arguments\n";
+    return 1;
+  }
+  if (argc > 4)
+  {
+    std::cerr << "Too many arguments\n";
+    return 1;
+  }
+  mode = atoi(argv[1]);
+  if (mode == 0 || std::strlen(argv[1]) > 1)
+  {
+    std::cerr << "First parameter is not a number\n";
+    return 1;
+  }
+  if (mode < 1 || mode > 2)
+  {
+    std::cerr << "First parameter is out of range\n";
+    return 1;
+  }
 }
