87bb8704ba5c1d9cf8901b8183af2498f561e507
martin0martinase <leonidcernikov43@gmail.com>
2025-11-03 23:51:52 +0700

    (2) Memory allocation

--- chernikov.leonid/P3/main.cpp
+++ chernikov.leonid/P3/main.cpp
@@ -13,29 +13,46 @@ int main (int argc, char ** argv)
   {
 
   // reading file_1: memory allocation
-  std::ifstream input(argv[2]);
-  if (std::ifstream.fail())
+    std::ifstream input(argv[2]);
+  if (!input.is_open())
   {
     std::cerr << "Failed to read file\n";
-    return 1;
+    return 2;
   }
-  char size[3] = {};
-  for (int i = 0; i < 3; ++i)
+  int rows, cols;
+  if (!(input >> rows >> cols))
   {
-    char ch;
-    if (file.get(ch))
-    {
-      size[i] = ch;
-    }
-    else
-    {
-      std::cerr << "failed to count the character\n"
-      return 2;
-    }
+    std::cerr << "Failed to read matrix dimensions\n";
+    return 2;
   }
-  int rows = static_cast<int>(size[0]);
-  int cols = static_cast<int>(size[2]);
-  argv[1] = 1 ? int a[10000] = {} : int * adyn = new int[rows * cols];
+  if (rows == 0 && cols == 0) {
+    std::cerr << "Empty matrix\n";
+    return 2;
+  }
+  if (rows < 0 || cols < 0) {
+    std::cerr << "Invalid matrix dimensions\n";
+    return 2;
+  }
+
+  // memory allocation
+  if (argv[1] == "1")
+  {
+    int a[10000] = {};
+  }
+  else if (argv[1] == "2")
+  {
+      int* nums = reinterpret_cast<int*>(malloc(sizeof(int) * (rows * cols)));
+      if (nums == nullptr)
+      {
+        std::cerr << "Memory allocation failed\n";
+        return 2;
+      }
+  }
+  else
+  {
+    std::cerr << "parameter 2 is set incorrectly";
+    return 2;
+  }
 
   // reading file_2: filling the matrix
 
@@ -47,7 +64,7 @@ int main (int argc, char ** argv)
   argv = 1 ?*/
 
   std::ofstream output(argv[3]);
-  output(output, ... ) << "\n" 
+  output(output, ... ) << "\n"
   if (std::ofstream.fail())
   {
     return 1;
