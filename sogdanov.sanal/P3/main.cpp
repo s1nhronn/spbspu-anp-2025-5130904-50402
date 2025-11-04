1ddb3dc924118b0b9c336879132f4b63341c730b
kuzRK <sanalsogdanov11188@gmail.com>
2025-11-04 18:30:05 +0300

    errors w ComLine

--- sogdanov.sanal/P3/main.cpp
+++ sogdanov.sanal/P3/main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
-int main()
+int main(int argc, char ** argv)
 {
+  if (argc < 4) {
+    std::cerr << "Not enough arguments\n";
+    return 1;
+  } else if (argc > 4) {
+    std::cerr << "Too many arguments\n";
+    return 1;
+  } else if (*argv[1] > 2) {
+    std::cerr << "First parameter is out of range\n";
+    return 1;
+  }
+
 }
