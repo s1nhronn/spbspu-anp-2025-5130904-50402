ca1e828a04d7e1318a77f9c1f36226aa9fe8c3ef
Jaroslaw-super1 <Jarafan3@gmail.com>
2025-11-04 15:07:29 +0500

    Check arguments

--- afanasev.yaroslav/P3/main.cpp
+++ afanasev.yaroslav/P3/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main ()
+int main (int argc, char ** argv)
 {
-    return 0;
+  if (argc == 4)
+  {
+    std::string arg = argv[1];
+    if (arg == "2")
+    {
+      std::cout << 22 << "\n";
+      // Динамический
+    } else if (arg == "1")
+    {
+      std::cout << 11 << "\n";
+      // Фиксированный
+    } else {
+      std::cerr << "Invalid first argument" << "\n";
+      return 1;
+    }
+  } else
+  {
+    std::cerr << "Incorrect quantity of arguments" << "\n";
+    return 1;
+  }
+  
+  
+  return 0;
 }
