ee17501dbe59ae3ec415ef1b8e204f67133ad942
Jaroslaw-super1 <Jarafan3@gmail.com>
2025-11-04 16:01:35 +0500

    Get mtx

--- afanasev.yaroslav/P3/main.cpp
+++ afanasev.yaroslav/P3/main.cpp
@@ -2,11 +2,32 @@
 #include <fstream>
 #include <string>
 
+//./"main" 2 input.txt output.txt
+
 int main (int argc, char ** argv)
 {
   if (argc == 4)
   {
     std::string arg = argv[1];
+    if (arg != "2" && arg != "1") {
+      std::cerr << "Invalid first argument" << "\n";
+      return 1;
+    }
+
+    size_t r = 0, c = 0;
+    int a = 0;
+
+    std::ifstream input(argv[2]);
+    input >> r >> c;
+    input >> a;
+
+    input.close();
+
+    std::cout << "file :" << r << c << a << "\n";
+
+    std::ofstream output(argv[3]);
+    //output << a;
+
     if (arg == "2")
     {
       std::cout << 22 << "\n";
@@ -15,9 +36,6 @@ int main (int argc, char ** argv)
     {
       std::cout << 11 << "\n";
       // Фиксированный
-    } else {
-      std::cerr << "Invalid first argument" << "\n";
-      return 1;
     }
   } else
   {
