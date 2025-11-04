6fb7466f24444e154ac86b75ed2b3afbaa0046d1
voyadger01 <voyadgerkx@gmail.com>
2025-11-04 12:32:35 +0300

    add strange check arguments

--- karpovich.dmitriy/P3/main.cpp
+++ karpovich.dmitriy/P3/main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <fstream>
 
-int main() {
-
+int main(int argc, char ** argv) {
+  if (argc > 4) {
+    std::cerr << "Too many arguments\n";
+    return 1;
+  } else if (argc < 4) {
+    std::cerr << "Not enough arguments\n";
+    return 1;
+  } else if ((argv[1][0] > '2' && argv[1][0] < '9')|| (argv[1][0] < '1' && argv[1][0] > '0')) {
+    std::cerr << "First parametr is out of range\n";
+    return 1;
+  } else if (argv[1][0] != '2' && argv[1][0] != '1') {
+    std::cerr << "First parametr is not a number\n";
+    return 1;
+  }
+  std::ifstream input(argv[2]);
+  std::ifstream input(argv[3]);
 }
