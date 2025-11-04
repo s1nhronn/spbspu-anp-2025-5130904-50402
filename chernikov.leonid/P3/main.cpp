5a4eb22f3c04ee5680ff69d54e140ff1d29317f1
martin0martinase <leonidcernikov43@gmail.com>
2025-11-04 15:12:04 +0700

    implementation down triangle matrix

--- chernikov.leonid/P3/main.cpp
+++ chernikov.leonid/P3/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <fstream>
-
-void output(const int * a, size_t c, size_t r);
+namespace chernikov
+{
+    void output (const int * a, size_t c, size_t r);
+    bool down_triangle_matrix (const int * a, int rows, int cols);
+}
 
 int main (int argc, char ** argv)
 {
+  char** end = argv + argc;
+  for (char** i = argv; i < end; ++i)
+  {
+    std::cout << *i << "\n";
+  }
   // checking the number of parameters
   if (argc < 4)
   {
@@ -55,13 +63,18 @@ int main (int argc, char ** argv)
   }
 
   // reading file_2: filling the matrix
+  for (size_t i = 0; i < (rows * cols); ++i)
+  {
+    input >> a[i];
 
-  input.close()
+  }
+  input.close();
 
-  /*int a[10000] ={}'
-  int * adyn = new int[rows * cols];
-
-  argv = 1 ?*/
+  //LWR-TRI-MTX
+  namespace cher = chernikov;
+  bool LWR_TRI_MTX = down_triangle_matrix (a, rows, cols);
+  std::ofstream output(argv[3]);
+  output << "LWR_TRI_MTX = " << LWR_TRI_MTX << "\n";
 
   std::ofstream output(argv[3]);
   output(output, ... ) << "\n"
@@ -75,4 +88,53 @@ int main (int argc, char ** argv)
 void output (std::ostream & out, const int * a, size_t c, size_t r)
 {
   return out << ... ;
+}
+
+bool down_triangle_matrix(const int* a, int rows, int cols)
+{
+  if (rows == cols)
+  {
+    if (&a[0] == 0)
+    {
+      int k = 0;
+      for (size_t i = 0; i < (rows - 1); ++i)
+      {
+        int line = rows * i;
+        for (size_t j = 0; j < (cols - k); ++j)
+        {
+          if (&a[j + line] != 0)
+          {
+              return false;
+          }
+        }
+        k++;
+      }
+      return true;
+    }
+    if (&a[cols - 1] == 0)
+    {
+      int k = 0;
+      for (size_t i = 0; i < (rows - 1); ++i)
+      {
+        int line = rows * i;
+        for (size_t j = (cols); j > k; --j)
+        {
+          if (&a[j - line] != 0)
+          {
+            return false;
+          }
+        }
+        k++;
+      }
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  else
+  {
+      return false;
+  }
 }
