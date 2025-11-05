0edb0c560b9595c2b92ffc1a205d3c8ec9acde15
Alastor20 <nik_d.007@mail.ru>
2025-11-05 12:57:08 +0300

    Basic logic

--- dirko.nikita/P3/main.cpp
+++ dirko.nikita/P3/main.cpp
@@ -3,7 +3,11 @@
 
 namespace dirko
 {
-
+  std::istream &staticInput(std::istream &in, int *arr, size_t r, size_t c);
+  std::istream &dinamicInput(std::istream &in, int *arr, size_t r, size_t c);
+  void var1(int *arr, int *res1, size_t r, size_t c);
+  bool var2(int *arr, size_t r, size_t c);
+  std::ostream &output(std::ostream &out, int *arr, size_t r, size_t c);
 }
 
 int main(int argc, char const **argv)
@@ -39,5 +43,32 @@ int main(int argc, char const **argv)
   {
     std::cerr << "Cant open input file\n";
     return 2;
+  }
+  size_t rows = 0, cols = 0;
+  fin >> rows >> cols;
+  if (mode == 1)
+  {
+    int arr[1000]{}, res1[1000]{};
+    bool res2 = false;
+    dirko::staticInput(fin, arr, rows, cols);
+    fin.close();
+    dirko::var1(arr, res1, rows, cols);
+    res2 = dirko::var2(arr, rows, cols);
+    std::ofstream fout(argv[3]);
+    dirko::output(fout, arr, rows, cols);
+  }
+  else
+  {
+    int *arr = nullptr;
+    int *res1 = nullptr;
+    bool res2 = false;
+    dirko::dinamicInput(fin, arr, rows, cols);
+    fin.close();
+    dirko::var1(arr, res1, rows, cols);
+    res2 = dirko::var2(arr, rows, cols);
+    std::ofstream fout(argv[3]);
+    dirko::output(fout, arr, rows, cols);
+    delete[] arr;
+    delete[] res1;
   }
 }
