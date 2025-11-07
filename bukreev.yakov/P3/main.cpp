9782508d01b96ac65154531fdea3b9aaf0119bbe
BukreevYas <bukreev.yas@edu.spbstu.ru>
2025-11-07 12:05:02 -0500

    Added argument checks.

--- bukreev.yakov/P3/main.cpp
+++ bukreev.yakov/P3/main.cpp
@@ -1,3 +1,41 @@
-int main()
-{}
+#include <iostream>
+#include <string>
 
+int main(int argc, char* argv[])
+{
+  if (argc < 4)
+  {
+    std::cerr << "Not enough arguments.\n";
+    return 1;
+  }
+
+  if (argc > 4)
+  {
+    std::cerr << "Too many arguments.\n";
+    return 1;
+  }
+
+  int arg1 = 0;
+  try
+  {
+    arg1 = std::stoi(argv[1]);
+  }
+  catch(const std::invalid_argument& e)
+  {
+    std::cerr << "First parameter is not a number.\n";
+    return 1;
+  }
+  catch(const std::out_of_range& e)
+  {
+    std::cerr << "First parameter is out of range.\n";
+    return 1;
+  }
+
+  if (arg1 != 1 && arg1 != 2)
+  {
+    std::cerr << "First parameter is out of range.\n";
+    return 1;
+  }
+
+  return 0;
+}
