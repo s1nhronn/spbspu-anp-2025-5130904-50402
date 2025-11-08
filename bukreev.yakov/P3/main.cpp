a548c117ca13585e764b31061f14efe642093530
BukreevYas <bukreev.yas@edu.spbstu.ru>
2025-11-08 14:10:04 -0500

    Added matrix input.

--- bukreev.yakov/P3/main.cpp
+++ bukreev.yakov/P3/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+
+namespace bukreev
+{
+  int mode = 0;
+
+  int* inputMatrix(std::istream& in, int* stackMatrix);
+  void deleteMatrix(const int* matrix);
+}
 
 int main(int argc, char* argv[])
 {
@@ -37,5 +46,71 @@ int main(int argc, char* argv[])
     return 1;
   }
 
+  bukreev::mode = arg1;
+
+  int* matrix = nullptr;
+  std::ifstream input(argv[2]);
+  if (arg1 == 1)
+  {
+    const size_t MTX_MAX_SIZE = 10000;
+    int stackMatrix[MTX_MAX_SIZE] = {};
+    matrix = stackMatrix;
+  }
+
+  try
+  {
+    matrix = bukreev::inputMatrix(input, matrix);
+  }
+  catch(const std::invalid_argument& e)
+  {
+    std::cerr << "Content of the input file is invalid: ";
+    std::cerr << e.what() << ".\n";
+    return 2;
+  }
+
+  bukreev::deleteMatrix(matrix);
+
   return 0;
+}
+
+int* bukreev::inputMatrix(std::istream& in, int* stackMatrix)
+{
+  int* matrix = nullptr;
+
+  int n = 0, m = 0;
+  in >> n >> m;
+
+  if (in.fail())
+  {
+    throw std::invalid_argument("No width or height of matrix");
+  }
+
+  if (mode == 1)
+  {
+    matrix = stackMatrix;
+  }
+  else
+  {
+    matrix = new int[n * m];
+  }
+
+  for (int i = 0; i < n * m; i++)
+  {
+    in >> matrix[i];
+  }
+
+  if (in.fail())
+  {
+    throw std::invalid_argument("Not enough elements of matrix");
+  }
+
+  return matrix;
+}
+
+void bukreev::deleteMatrix(const int* matrix)
+{
+  if (mode == 2)
+  {
+    delete[] matrix;
+  }
 }
