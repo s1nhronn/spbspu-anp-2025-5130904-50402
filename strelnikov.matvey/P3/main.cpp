5fe784c27597789c83b14c2130100b82103f1bbc
hihikomori <ghhhhhnika@gmail.com>
2025-11-12 03:42:43 +0300

    done with code, starting to do fixes

--- strelnikov.matvey/P3/main.cpp
+++ strelnikov.matvey/P3/main.cpp
@@ -28,38 +28,6 @@ void staticmtx(std::ifstream& out, int ** a, size_t r, size_t c){
   }
 }
 
-void lftBotCnt(int** mtx, int r, int c){
-
-}
-
-size_t cntNzrDig(int ** mtx, size_t r, size_t c){
-  size_t min = (r < c)?r:c;
-  int cnt = 0;
-  for (size_t i = 1; i < min; ++i) {
-    int tr = 1;
-    size_t ecri = i;
-    for (size_t j = 0; j < min - i; ++j) {
-      if (mtx[j][ecri] == 0) {
-        tr = 0;
-      }
-      ++ecri;
-    }
-    cnt += tr;
-  }
-  for (size_t i = 1; i < min; ++i) {
-    size_t ecri = i;
-    int tr = 1;
-    for (size_t j = 0; j < min - i; ++j) {
-      if (mtx[ecri][j] == 0) {
-        tr = 0;
-      }
-      ++ecri;
-    }
-    cnt += tr;
-  }
-  return cnt;
-}
-
 int ** dynamicmtx(std::ifstream& out, size_t r, size_t c){
   int ** a = nullptr;
   size_t cnt = 0;
@@ -92,6 +60,66 @@ int ** dynamicmtx(std::ifstream& out, size_t r, size_t c){
   return a;
 }
 
+void lftBotCnt(int** mtx, int r, int c){
+  size_t t = 0, l = 0;
+  size_t b = r - 1, ri = c - 1;
+  int cnt = 1;
+
+  while (l <= ri && t <= b) {
+    for (int j = l; j <= r; ++j) {
+      mtx[b][j] += cnt++;
+    }
+    --b;
+
+    for (int i = b; i >= t; --i) {
+      mtx[i][r] += cnt++;
+    }
+    --ri;
+
+    if (t <= b) {
+      for (int j = ri; j >= l; --j) {
+        mtx[t][j] += cnt++;
+      }
+      ++t;
+    }
+
+    if (l <= ri) {
+      for (int i = t; i <= b; ++i) {
+        mtx[i][l] += cnt++;
+      }
+      ++l;
+    }
+  }
+}
+
+size_t cntNzrDig(int ** mtx, size_t r, size_t c){
+  size_t min = (r < c)?r:c;
+  int cnt = 0;
+  for (size_t i = 1; i < min; ++i) {
+    int tr = 1;
+    size_t ecri = i;
+    for (size_t j = 0; j < min - i; ++j) {
+      if (mtx[j][ecri] == 0) {
+        tr = 0;
+      }
+      ++ecri;
+    }
+    cnt += tr;
+  }
+  for (size_t i = 1; i < min; ++i) {
+    size_t ecri = i;
+    int tr = 1;
+    for (size_t j = 0; j < min - i; ++j) {
+      if (mtx[ecri][j] == 0) {
+        tr = 0;
+      }
+      ++ecri;
+    }
+    cnt += tr;
+  }
+  return cnt;
+}
+
 void doall(int pr, const char * outf, const char * inf){
   std::ifstream out;
   out.open(outf);
@@ -103,36 +131,45 @@ void doall(int pr, const char * outf, const char * inf){
   int ** mtx = nullptr;
   size_t r, c;
   out >> r >> c;
-  if(std::cin.fail()){
+  std::cout << "get r and c\n";
+  if(out.fail()){
     throw std::logic_error("Wrong format");
   }
   try{
     if(pr == 1){
+      int * mtx[10000];
       staticmtx(out,mtx,r,c);
     }else{
       mtx = dynamicmtx(out,r,c);
     }
   }catch(const std::logic_error &e){
-    rm(mtx, r);
+    throw;
+  }catch(const std::bad_alloc &e){
     throw;
   }
+  std::cout << "get mtx";
   out.close();
   std::ofstream in;
   in.open(inf);
   if(!in.is_open()){
+    if(pr == 2){rm(mtx, r);}
     throw std::logic_error("Cannot open in file\n");
   }
   size_t dig = cntNzrDig(mtx, r, c);
+  std::cout << dig;
   in << dig;
   lftBotCnt(mtx, r, c);
   for(size_t i = 0; i < r; ++i){
     for(size_t j = 0; j < r; ++j){
       in << mtx[i][j] << ' ';
+      std::cout << mtx[i][j] << ' ';
     }
+    in << '\n';
     std::cout << '\n';
   }
+  std::cout << "do tasks\n";
   in.close();
-  rm(mtx, r);
+  if(pr == 2){rm(mtx, r);}
 }
 
 int main(int argc, char const *argv[]){
@@ -148,6 +185,7 @@ int main(int argc, char const *argv[]){
     int pr;
     try{
       pr = std::stoi(argv[1]);
+      std::cout << pr;
     } catch (const std::invalid_argument& e) {
       std::cerr << "First argument is not a number\n";
       ex_code = 1;
@@ -163,7 +201,7 @@ int main(int argc, char const *argv[]){
     }
     if(ex_code == 0){
       try{
-        //doall(pr, argv[2], argv[3]);
+        doall(pr, argv[2], argv[3]);
       }catch(...){
         ex_code = 3;
       }
