23918a708f65cea4c4b7ae4e0bc7f076eabd690e
Stave143 <staveplay@mail.ru>
2025-11-12 21:10:04 +0300

    fix args and add some err messeges

--- permyakov.sergey/P3/main.cpp
+++ permyakov.sergey/P3/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <fstream>
 
-int main(int num, char ** input, char ** output)
+int main(int argc, char ** argv)
 {
-  (void)num;
-  (void)input;
-  (void)output;
+  if(argc < 4){
+    std::cerr << "Not enough arguments\n";
+    return 1;
+  }
+  if(!(*argv[1] == '1' || *argv[1] == '2')){
+    std::cerr << "first parametr is out of range\n";
+    return 1;
+  }
   return 0;
 }
