377a60ca5afde072181c4fab8c2675dc344b78b1
Sp0146001 <sp0146001@gmail.com>
2025-11-13 02:32:13 +0300

    Added function for static array, rm func finished & added body for dynamic array

--- petrov.sasha/P3/main.cpp
+++ petrov.sasha/P3/main.cpp
@@ -1,46 +1,80 @@
 #include <iostream>
 #include <fstream>
-/*
-Принимаю три значения num input output
-Вход - двумерный целочисленный массив, первые два - строки столбцы, потом эл-ты
-output - выход, куда надо вывести массив с результатом
-*/
 namespace petrov
 {
-  void rm(int** a, size_t rows)
+  void rm(int** mtx, size_t rows)
   {
-  
+    for (size_t i = 0; i < rows, i++){
+      delete[] a[i];
+    }
+    delete[] a;
   }
-  size_t LFT_BOT_CNT(size_t** a, size_t r, size_t c)
+
+  void stat(std::ifstream& out, int** a, size_t rows, size_t cols)
+  {
+
+  }
+
+  void dyn(std::ifstream& out, size_t rows, size_t cols)
+  {
+    int** a = nullptr;
+     size_t qnt_numb = 0;
+    try {
+      a = new int*[rows];
+      for (size_t i = 0; i < rows; ++i)
+      {
+        a[i] = new int[cols];
+      }
+    } catch (const std::bad_alloc& e) {
+      rm(a, rows);
+      throw;
+    }
+    for (size_t i = 0; i < rows; ++i)
+    {
+      for (size_t j = 0; j < cols; ++j)
+      {
+        qnt_numb++;
+        out >> a[i][j];
+      }
+    }
+    if (qnt_numb != rows*cols))
+    {
+      throw std::lenght_error("Too many/few arguments for array\n");
+  }
+
+  size_t LFT_BOT_CNT(int** a, size_t r, size_t c)
   {
     size_t counter = 0;
     size_t up = 0, down = --r, left = 0, right = --c;
-    while(up <= down && left <= right)
+    while (up <= down && left <= right)
     {
-      for(size_t i = left; i < right; ++i)
+      for (size_t i = left; i < right; ++i)
       {
         counter++;
         a[down][i]+=counter;
       }
       down--;
-      if(left<=right){
-        for(size_t i = down; i >= up; --i)
+
+      if (left<=right){
+        for (size_t i = down; i >= up; --i)
         {
           counter++;
           a[i][right]+=counter;
         }
         right--;
       }
-      if(up<=down)
+
+      if (up<=down)
       {
-        for(size_t i = right; i>=left; --i)
+        for (size_t i = right; i>=left; --i)
         {
           counter++;
           a[up][i]+=counter;
         }
         up++;
       }
-      for(size_t i = up; i <= down; ++i)
+
+      for (size_t i = up; i <= down; ++i)
       {
         counter++;
         a[i][left]+=counter;
