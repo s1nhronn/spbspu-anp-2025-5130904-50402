837e305e1e0793e8d0c78a7dd8f48d072998ba8e
s1nhronn <silekatik@yandex.ru>
2025-11-12 17:01:29 +0300

    Some changes and add func input

--- shirokov.arsenii/P3/main.cpp
+++ shirokov.arsenii/P3/main.cpp
@@ -1,41 +1,74 @@
 #include <fstream>
-#include <ostream>
+#include <iostream>
+#include <stdexcept>
 
 size_t lengthInput(char *file);
-void input(const std::istream &in, const int *m);
-std::ostream &output(const std::ostream &out, const int *m, size_t a, size_t b);
+void input(std::istream &in, int *m, size_t lng);
+std::ostream &output(std::ostream &out, const int *m, size_t a, size_t b);
 
 int main(int argc, char **argv)
 {
-  char *num = argv[1];
+  if (argc < 4)
+  {
+    std::cerr << "Not enough arguments\n";
+    return 1;
+  }
+  if (argc > 4)
+  {
+    std::cerr << "Too many arguments\n";
+  }
+  // TODO: Условие для argv[1] > 2 и для argv[1] -  не число
 
+  char *num = argv[1];
+  size_t lng = 0;
+  try
+  {
+    lng = lengthInput(argv[2]);
+  } catch (const std::logic_error &e)
+  {
+    std::cerr << e.what() << '\n';
+    return 2;
+  }
   int a[10000] = {};
-  int *b = new int[lengthInput(argv[2])];
+  int *b = new int[lng];
   std::ifstream in(argv[2]);
-  int *m = nullptr;
+  size_t m, n;
+  in >> m >> n;
+
+  int *massive = nullptr;
   if (num[0] == '1')
   {
-    input(in, a);
-    m = a;
+    input(in, a, lng);
+    massive = a;
     delete[] b;
   }
   else
   {
-    input(in, a);
-    m = b;
+    input(in, a, lng);
+    massive = b;
   }
 }
 
 size_t lengthInput(char *file)
 {
   std::ifstream in(file);
-  size_t count = 0;
-  int temp = 0;
-  in >> temp >> temp;
-  while (in >> temp)
+  size_t a, b;
+  in >> a >> b;
+  if (in.fail())
   {
-    count++;
+    throw std::logic_error("Size of massive is not correct");
   }
-  in.close();
-  return count;
+  return a * b;
+}
+
+void input(std::istream &in, int *m, size_t lng)
+{
+  for (size_t i = 0; i < lng; i++)
+  {
+    in >> m[i];
+    if (in.fail())
+    {
+      throw std::logic_error("Values in massive is not correct");
+    }
+  }
 }
