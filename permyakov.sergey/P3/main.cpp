8f348fcb7d662f57dbc16364a1909879376fa8f3
Stave143 <staveplay@mail.ru>
2025-11-12 22:06:52 +0300

    added full error 1 checkout

--- permyakov.sergey/P3/main.cpp
+++ permyakov.sergey/P3/main.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
 #include <fstream>
 
-int main(int argc, char ** argv)
+bool isErr(int argc, char ** argv)
 {
   if(argc < 4){
     std::cerr << "Not enough arguments\n";
-    return 1;
+    return true;
   }
-  if(!(*argv[1] == '1' || *argv[1] == '2')){
-    std::cerr << "first parametr is out of range\n";
+  if(argc > 4){
+    std::cerr << "Too many arguments\n";
+    return true;
+  }
+  if(argv[1][0] != '1' && argv[1][0] != '2'){
+    std::cerr << "First argument is not correct\n";
+    return true;
+  }
+  std::ifstream input(argv[2]);
+  if(input.fail()){
+    std::cerr << "Second argument does not exist between files\n";
+    return true;
+  }
+  std::ifstream output(argv[3]);
+  if(input.fail()){
+    std::cerr << "Third argument does not exist between files\n";
+    return true;
+  }
+  return false;
+}
+
+int main(int argc, char ** argv)
+{
+  if(isErr(argc, argv)){
     return 1;
   }
   return 0;
