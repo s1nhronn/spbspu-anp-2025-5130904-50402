f03f59dd2580d3ac9551abf05a2aba923e825e72
BukreevYas <bukreev.yas@edu.spbstu.ru>
2025-11-13 07:23:11 -0500

    Added grow function.

--- bukreev.yakov/P4/main.cpp
+++ bukreev.yakov/P4/main.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 #include <cstdlib>
+#include <cstring>
 
 namespace bukreev
 {
   char* inputString();
   void deleteString(const char* str);
+
+  char* growString(const char* oldStr, size_t* capacity);
 }
 
 int main()
 {
   char* str = nullptr;
-  
+
   try
   {
     str = bukreev::inputString();
   }
   catch(const std::bad_alloc& e)
   {
-    std::cerr << "No memory" << '\n';
+    std::cerr << "Not enough memory.\n";
     return 1;
   }
 
@@ -43,17 +46,16 @@ char* bukreev::inputString()
   {
     if (i >= capacity)
     {
-      //grow
+      buffer = growString(buffer, &capacity);
     }
 
     std::cin >> buffer[i];
     if (buffer[i] == '\n')
     {
-      break;
       i++;
       if (i >= capacity)
       {
-        //grow
+        buffer = growString(buffer, &capacity);
       }
 
       buffer[i] = 0;
@@ -69,4 +71,27 @@ char* bukreev::inputString()
 void bukreev::deleteString(const char* str)
 {
   free(const_cast<char*>(str));
+}
+
+char* bukreev::growString(const char* oldStr, size_t* capacity)
+{
+  const double growRatio = 1.5;
+
+  size_t oldCapacity = *capacity;
+  size_t newCapacity = oldCapacity * growRatio;
+
+  *capacity = newCapacity;
+
+  char* newStr = reinterpret_cast<char*>(malloc(newCapacity * sizeof(char)));
+  if (!newStr)
+  {
+    deleteString(oldStr);
+    throw std::bad_alloc();
+  }
+
+  std::strncpy(newStr, oldStr, oldCapacity);
+
+  deleteString(oldStr);
+
+  return newStr;
 }
