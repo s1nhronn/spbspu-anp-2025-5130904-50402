7c4f026482fa184b86ad5dd875d8952eb0a9b716
BukreevYas <bukreev.yas@edu.spbstu.ru>
2025-11-13 06:52:44 -0500

    Added template fot input and delete functions.

--- bukreev.yakov/P4/main.cpp
+++ bukreev.yakov/P4/main.cpp
@@ -1,2 +1,57 @@
+#include <iostream>
+#include <cstdlib>
+
+namespace bukreev
+{
+  char* inputString();
+  void deleteString(const char* str);
+}
+
 int main()
-{}
+{
+  char* str = bukreev::inputString();
+  bukreev::deleteString(str);
+}
+
+char* bukreev::inputString()
+{
+  const size_t initialSize = 4;
+
+  size_t capacity = initialSize;
+
+  char* buffer = reinterpret_cast<char*>(malloc(initialSize * sizeof(char)));
+
+  std::cin >> std::noskipws;
+
+  size_t i = 0;
+  while (true)
+  {
+    if (i >= capacity)
+    {
+      //grow
+    }
+
+    std::cin >> buffer[i];
+    if (buffer[i] == '\n')
+    {
+      break;
+      i++;
+      if (i >= capacity)
+      {
+        //grow
+      }
+
+      buffer[i] = 0;
+      break;
+    }
+
+    i++;
+  }
+
+  return buffer;
+}
+
+void bukreev::deleteString(const char* str)
+{
+  free(const_cast<char*>(str));
+}
