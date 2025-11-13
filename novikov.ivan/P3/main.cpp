20f9320b3d5d62595832e61bb3bad7825c32cf05
Kayv1e <krutojpacanivan@gmail.com>
2025-11-13 22:37:45 +0300

    Added matrix creation/removal

--- novikov.ivan/P3/main.cpp
+++ novikov.ivan/P3/main.cpp
@@ -1,7 +1,19 @@
+#include <cstddef>
 #include <exception>
+#include <fstream>
 #include <iostream>
+#include <new>
 #include <stdexcept>
 #include <string>
+
+void remove(int ** mtx, size_t r)
+{
+  for (size_t i = 0; i < r; ++i) {
+    delete[] mtx[i];
+  }
+  delete[] mtx;
+}
+
 int main(int argc, char ** argv)
 {
   if (argc < 4) {
@@ -27,12 +39,60 @@ int main(int argc, char ** argv)
     return 1;
   }
 
+  std::ifstream input(argv[2]);
+  size_t rows, cols;
+  input >> rows >> cols;
+
+  if (rows * cols > 10000) {
+    std::cerr << "Matrix is too large\n";
+    return 2;
+  }
+
   try {
     if (firstarg == 1) {
-      // static array function
+      size_t count = 0;
+      int mtx[10000][10000];
+
+      for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < cols; ++j) {
+          input >> mtx[i][j];
+          ++count;
+        }
+      }
+
+      if (rows * cols != count) {
+        std::cerr << "Wrong matrix format\n";
+        return 2;
+      }
     } else if (firstarg == 2) {
-      // dynamic array function
+      int ** mtx = new int * [rows];
+
+      for (size_t i = 0; i < rows; ++i) {
+        try {
+          mtx[i] = new int[cols];
+        } catch (std::bad_alloc &) {
+          remove(mtx, i);
+          throw;
+        }
+      }
+
+      size_t count = 0;
+
+      for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < cols; ++j) {
+          input >> mtx[i][j];
+          ++count;
+        }
+      }
+
+      if (rows * cols != count) {
+        std::cerr << "Wrong matrix format\n";
+        return 2;
+      }
     }
+  } catch (std::bad_alloc &) {
+    std::cerr << "Memory can not be allocated\n";
+    return 2;
   } catch (std::exception & e) {
     std::cerr << e.what() << "\n";
     return 2;
