33857da62dfa28b5b50b48024f99163a8cc87ea0
Omar702140 <rnmp4801@gmail.com>
2025-11-14 01:27:54 +0300

    Poka tak

--- terentev.roman/P3/main.cpp
+++ terentev.roman/P3/main.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+
+bool anum(char * a);
 
 int main(int argc, char ** argv)
 {
     std::ifstream input(argv[2]);
-    std::ifstream output(argv[3]);
+    if (argc < 4)
+    {
+        std::cerr << "Not enough arguments\n";
+        return 1;
+    }
+    if (argc > 4)
+    {
+        std::cerr << "Too many arguments\n";
+        return 1;
+    }
+    if (!anum (argv[1])) {
+        std::cerr << "First parameter is not a number\n";
+        return 1;
+    }
+    if (std::stoi(std::string(argv[1]))>2 || std::stoi(std::string(argv[1])) < 1) {
+        std::cerr << "First parameter is out of range\n";
+    }
+    std::ofstream output(argv[3]);
+    std::cout << "\n";
+}
+
+bool anum(char * a) {
+
 }
