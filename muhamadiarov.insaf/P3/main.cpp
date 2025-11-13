8b8b1289b52d509e7168a017c6388a7b788acbb9
InsafM1 <muhamadiarovinsaf0@gmail.com>
2025-11-13 03:04:40 +0300

    removed the vector and the memory-dividing function

--- muhamadiarov.insaf/P3/main.cpp
+++ muhamadiarov.insaf/P3/main.cpp
@@ -9,14 +9,21 @@
 
 namespace muhamadiarov
 {
+  struct matric_info
+  {
+    int* start;
+    int rows;
+    int colons;
+  }
+
   int max_int(); 
-  int min_int(); 
-  void toChoiseMemmory(std::vector<int> data, char* argv[]); 
-  void fll_inc_wav(int* ptr, char output, size_t count, int n, int k); 
-  void out(int* ptr,char output, size_t count, int n, int k, int* res1, long long res2); 
+  int min_int();
+ 
+  int* fll_inc_wav(matric_info matrix); 
+  void out(matric_info matrix, const char* output, int* res1, long long int res2); 
   long long toFindMaxRight(int* ptr, int order); 
   long long toFindMaxinLeft(int* ptr, int order); 
-  long long max_sum_mdg(int* ptr, char output, int order); 
+  long long max_sum_mdg(matric_info matrix, int order); 
   bool tocheckparam(int argc, char* argv[]); 
   void tocoutargv(int argc, char * argv[]);
 }
@@ -25,146 +32,141 @@ bool isOutfileOpened = true;
 
 int main(int argc, char *argv[]) 
 {
+  namespace muh = muhamadiarov;
   bool checkparam = muh::tocheckparam(argc, argv); //проверка параметров main
   if (!checkparam)
   {
     return 1;
   }
-  namespace muh = muhamadiarov;
   std::ifstream input(argv[2]); // открытие файла
   if (!input) // открылся ли файл?
   {
     std::cerr << "Error is openning file\n";
     return 2;
   }
-  std::vector<int> data; // вектор как временное хранилище
-  std::string line;
-  int number = 0;
-  size_t c = 0;
-  if (!std::getline(input, line)) // не пустой ли файл?
+ 
+  if (input.eof())
   {
-    std::cerr << "Error in standart stream error\n";
+    std::cerr << "The file is empty\n";
     return 2;
   }
-  while (std::getline(input, line)) // читаем построчно
+  size_t count = 0;
+  while (input >> number)
   {
-    std::stringstream iss(line);
-    try
-    {
-      while (iss >> number) // считываем числа из строки
-      {
-        bool ch1 =  typeid(number).name() != typeid(int).name();
-        bool ch2 = number < min_int || number > max_int;
-        if (ch1 || ch2) // проверка number
-        {
-          std::cerr << "Error in standart stream of error\n";
-          return 2;	
-        }
-        data.push_back(number);
-      }
-    }
-    catch(std::bad_alloc &e) // если произошло ошибка при работе vector
+    ++count;
+  }
+
+  if (count == 0)
+  {
+    std::cerr << "The file is empty\n";
+    return 2;
+  }
+  else if (count < 2)
+  {
+    std::cerr << "Not enough vaules\n";
+    return 2;
+  }
+
+  input.clear();
+  input.seekg(0);
+
+  if (argv[1] == '1')
+  {
+    int array[count];
+    int* ptr = &array[0];
+  }
+  else if (argv[1] == '2')
+  {
+    int* ptr = static_cast<int*>(malloc(count * sizeof(int)));
+    if (ptr == nullptr)
     {
       std::cerr << "Failed to allocate memory\n";
-      data.clear();
       return 1;
     }
-    if (data[0] * data[1] != data.size() - 2) // правильно ли даны размеры матрицы?
-    {
-      std::cerr << "Error in standart stream of error\n";
-      data.clear();
-      return 2;
-    }
-    else
-    {
-      if (data[0] == data[1] && data[0] == 0) // если матрица размера 0 0
-      {
-	int* ptr = data;
-        out(ptr, argv[3], 0, 0, 0, ptr, 0);
-      }
-      else
-      {
-        try
-	{
-          muh::toChoiseMemmory(data, argv) // все дальнейшие действия находятся там
-	}
-	catch(...) // Если malloc не смог выделить память
-	{
-	  std::cerr << "Failed to allocate memory\n";
-	  data.clear();
-	  return 1;
-	}
-      }
-    }
-    data.clear();
   }
-  if (!isOutfileOpened) // если файл output не открылся
+  
+  int number = 0;
+  size_t k = 0;
+  size_t k1 = 0;
+  int r = 0, int c = 0;
+  while (input >> number)
   {
-    std::cerr << "Error openning file\n";
-    return 1; 
+    bool ch1 =  typeid(number).name() != typeid(int).name();
+    bool ch2 = ch1 || number < min_int || number > max_int;
+    if (ch2) // проверка number
+    {
+      std::cerr << "Incorrect type for number\n";
+      if (argv[1] == '2')
+      {
+        free(array);
+      }
+      return 2;	
+    }
+
+
+    if (k1 == 0)
+    {
+      r = number;
+    }
+    else if (k1 == 1)
+    {
+      c = number;
+    }
+    else if (input.peek() == '\n')
+    {
+      if (k1 - 1 != r * c)
+      {
+        std::cerr << "Wrong size matric\n";
+	if (argv[1] == 1)
+	{
+	  free(array);
+	}
+	return 2;
+      }
+    }
+
+
+    *(ptr + k) = number;
+    ++k;
   }
   input.close();
+  // c - count elements in file
+  // r  - rows
+  // c - colons
+  int* result_1 = nullptr;
+  long long int result_2 = 0;
+  muh::matric_info matrix;
+  for (size_t i = 0; i < c; ++i)
+  {
+    r = *(ptr + i);
+    c = *(ptr + i + 1);
+    if (i + 2 < c)
+    {
+      matrix{ptr + i + 2, r, c};
+    }
+    else if (r == 0 && c == 0)
+    {
+      matrix{ptr + i, 0, 0};
+      muf::out(matrix, argv[3], result_1, result_2);
+    } 
+    result_1 = muf::fll_inc_wav(matrix);
+    int order = std::min(r, c);
+    result_2 = muf::max_sum_mdg(matrix, order); 
+    muf::out(matrix, argv[3], result_1, result_2);
+    i += r * c + 2; 
+  }
+  if (argv[3] == '2')
+  {
+    free(array);
+  }
   return 0;
 }
 
-namespace muh = muhamadiarov;
-
-void muh::toChoiseMemmory(std::vector<int> data, char* argv[])
+int* muhamadiarov::fll_inc_wav(muhamadiarov::matric_info matrix, char output)
 {
-  int n = data[0], k = data[1];
-  size_t count = data[0]*data[1];
-  if (argv[1] == 1)
-  {
-    int array[count];
-    for (int i = 0; i < count; ++i)
-    {
-      array[i] == data[i+2];
-    }
-    int* begin = array;
-    long long resultFirstTask = 0ll;
-    order = std::min(n, k);
-    int* resultSecondTask[order];
-    resultFirstTask = muh::fll_inc_wav(begin, argv[3], count, n, k);
-    resultSecondTask = muh::max_sum_mdg(begin, argv[3], count, order);
-  }
-  else
-  {
-    int* arr = static_cast<int*>(malloc(count * sizeof(int)));
-    if (arr == nullptr)
-    {
-      throw;
-    }
-    int* begin = arr;
-    for (size_t i = 0; i < count; ++i)
-    {
-      *(begin+i) = data[i+2];
-    }
-    long long resultFirstTask = 0ll;
-    if (n != k)
-    {
-      order = std::min(n, k);
-      int* resultSecondTask[] = static_cast<int>malloc(order * sizeof(int));
-      begin = resultSecondTask;
-      if (resultSecondTask == nullptr)
-      {
-        free(arr);
-        throw;
-      }
-      for (size_t i = 1; i <= order; ++1)
-      {
-        *(begin + i * order + i) = arr[i*order + i];
-      }
-    }
-    resultFirstTask = muh::fll_inc_wav(begin, argv[3], count, n, k);
-    resultSecondTask = muh::max_sum_mdg(begin, argv[3], count, order);
-    free(arr);
-    free(resultFirstTask);
-  }
-  muh::out(begin, count, n, k, resultFirstTask, resultSecondTask);
-}
-
-void muh::fll_inc_wav(int* ptr, char output, size_t count, int n, int k)
-{
+  namespace muh = muhamadiarov;
+  int n = matrix.rows;
+  int k = matrix.colons;
   int medium_n = n%2?(n/2+1):(n/2);
   int medium_k = k%2?(k/2+1):(k/2);
   for (size_t i = 0; i < k; ++i)
@@ -182,26 +184,27 @@ void muh::fll_inc_wav(int* ptr, char output, size_t count, int n, int k)
         k1 = medium_k%2?(k1-(k1-medium_k)*2):(medium_k-(k1-medium_k+1));
       }
       p = std::min(k1, n1);
-      *(ptr + i*n + j) += p;
+      *(ptr + j*n + i) += p;
     }
   }
-  muf::out(ptr, output, count, n, k);
+  muh::out(ptr, output, count, n, k);
 }
 
 
-long long muh::max_sum_mdg(int* ptr, char output, int order)
+long long muhamadiarov::max_sum_mdg(muhamadiarov::matric_info matrix, int order)
 {
-  long long max_result = 0ll;
-  long long par_branch_right = muh::toFindMaxinRight(ptr, order);
-  long long par_branch_left = muh::toFindMaxinLeft(ptr, order);
+  namespace muh = muhamadiarov;
+  long long max_result = 0;
+  long long par_branch_right = muh::toFindMaxinRight(matrix.ptr, order);
+  long long par_branch_left = muh::toFindMaxinLeft(matrix.ptr, order);
   max_result = std::max(par_branch_right, par_branch_left);
   return max_result;
 }
 
-long long muh::toFindMaxinLeft(int* ptr, int order)
+long long muhamadiarov::toFindMaxinLeft(int* ptr, int order)
 {
-  long long max_r = 0ll;
-  long long result = 0ll;
+  long long max_r = 0;
+  long long result = 0;
   int n = order, k = order;
   while (n > 0 && k > 0)
   {
@@ -216,10 +219,10 @@ long long muh::toFindMaxinLeft(int* ptr, int order)
   return max_r;
 }
 
-long long muh::toFindMaxRight(int* ptr, int order)
+long long muhamadiarov::toFindMaxRight(int* ptr, int order)
 {
-  long long max_r = 0ll;
-  long long result = 0ll;
+  long long int max_r = 0;
+  long long int result = 0;
   int n = order, k = order;
   int k1 = 2;
   while (k >= k1)
@@ -235,8 +238,11 @@ long long muh::toFindMaxRight(int* ptr, int order)
   return max_r;
 }
 
-void muh::out(int* ptr,char output, size_t count, int n, int k, int* res1, long long res2)
+void muhamadiarov::out(muhamadiarov::matric_info matrix,char output, int* res1, long long int res2)
 {
+  int n = matrix.rows;
+  int k = matrix.colons; 
+  size_t count = n * k;
   std::ofstream outfile(output);
   if (!outfile)
   {
@@ -256,13 +262,13 @@ void muh::out(int* ptr,char output, size_t count, int n, int k, int* res1, long 
       {
         for (size_t i; i < n*n; ++i)
         {
-          output << *(res1 + i) << " ";
+          outfile << *(res1 + i) << " ";
         }
         outfile << "\n";
       }
       else
       {
-        output << res2 << "\n";
+        outfile << res2 << "\n";
       }
     }
     outfile.close();
@@ -270,7 +276,7 @@ void muh::out(int* ptr,char output, size_t count, int n, int k, int* res1, long 
 }
 
 
-void muh::tocoutargv(int argc, char* argv[])
+void muhamadiarov::tocoutargv(int argc, char* argv[])
 {
   for (size_t i = 0; i < argc; ++i)
   {
@@ -278,8 +284,9 @@ void muh::tocoutargv(int argc, char* argv[])
   }
 }
 
-bool muh::tocheckparam(int argc, char* argv[])
+bool muhamadiarov::tocheckparam(int argc, char* argv[])
 {
+  namespace muh = muhamadiarov;
   if (argc < 4)
   {
     muh::tocoutargv(int argv, char* argv[]);
@@ -308,13 +315,13 @@ bool muh::tocheckparam(int argc, char* argv[])
 }
 
 
-int muh::max_int()
+int muhamadiarov::max_int()
 {
   using namespace std;
   return numeric_limits<int>::max();
 }
 
-int muh::min_int()
+int muhamadiarov::min_int()
 {
   using namespace std;
   return numeric_limits<int>::min();
