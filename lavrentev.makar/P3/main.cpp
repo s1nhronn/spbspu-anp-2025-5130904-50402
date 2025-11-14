25423df29aa3dfe91d9a31601f4cd6f7b67d1942
lavrentev.makar <makar.lavrentev@yandex.ru>
2025-11-15 01:59:13 +0300

    Full code rebuild 22 hours before deadline :)

--- lavrentev.makar/P3/main.cpp
+++ lavrentev.makar/P3/main.cpp
@@ -2,19 +2,42 @@
 #include <iostream>
 #include <string>
 
+bool is_digit(char * str);
+int str_to_int(char * str);
+int my_pow(int a, int b);
+size_t cnt_loc_min(int * arr, size_t x, size_t y);
+size_t num_col_lsr(int * arr, size_t x, size_t y);
+void input_file(std::istream &in, int *m, size_t lng);
+
 int main(int argc, char **argv)
 {
-  if (argc != 4)
+  if (argc < 4)
   {
-    std::cerr << "Input error" << "\n";
+    std::cerr << "Not enough arguments\n";
     return 1;
   }
-
-  std::string s1 = argv[1];
-
-  if(s1 != "1" && s1 != "2")
+  if (argc > 4)
   {
-    std::cerr << "First parameter is out of range";
+    std::cerr << "Too many arguments\n";
+    return 1;
+  }
+  if (!is_digit(argv[1]))
+  {
+    std::cerr << "First parameter is not a number\n";
+    return 1;
+  }
+  int num = 0;
+  try
+  {
+    num = str_to_int(argv[1]);
+  } catch (const std::overflow_error &e)
+  {
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
+  if (num != 1 && num != 2)
+  {
+    std::cerr << "First parameter is out of range\n";
     return 1;
   }
 
@@ -41,34 +64,106 @@ int main(int argc, char **argv)
   }
 
   size_t total = x * y;
+
+  int arr2[10000] = {};
   int total_for_memory = static_cast<int>(x) * static_cast<int>(y);
   int *arr = static_cast<int*>(malloc(total_for_memory * sizeof(int)));
-  if (arr == nullptr)
-  {
-    std::cerr << "Memory allocation fail" << "\n";
-    return 1;
-  }
-  size_t size_arr = 0;
-  for (size_t i = 0; i < total; ++i)
-  {
-    int k;
-    if(!(input >> k))
-    {
-      break;
-    }
-    arr[i] = k;
-    ++size_arr;
-  }
-  input.close();
 
-  if (size_arr != total)
+  size_t ans_2;
+  size_t ans_11;
+
+  int *matrix = nullptr;
+  try
   {
+    if (num == 1)
+    {
+      input_file(input, arr2, total);
+      matrix = arr2;
+      free(arr);
+    }
+    else
+    {
+      input_file(input, arr, total);
+      matrix = arr;
+    }
+  } catch (const std::logic_error &e)
+  {
+    std::cerr << e.what() << '\n';
     free(arr);
-    std::cerr << "Uncorrect file" << "\n";
     return 2;
   }
 
-  int ans_2 = 0;
+  ans_2 = cnt_loc_min(matrix, x, y);
+  ans_11 = num_col_lsr(matrix, x, y);
+
+  std::ofstream output;
+  output.open(argv[3]);
+
+  output << "Answer for var_2: " << ans_2 << "\n";
+  output << "Answer for var_11: " << ans_11 << "\n";
+  if(num == 2)
+  {
+    free(arr);
+  }
+}
+
+
+
+bool is_digit(char * str)
+{
+  size_t len = std::strlen(str);
+  for (size_t i = 0; i < len; i++)
+  {
+    if (!('0' <= str[i] && str[i] <= '9'))
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+int str_to_int(char *str)
+{
+  int res = 0;
+  int iLimit = std::numeric_limits<int>::max();
+  size_t len = std::strlen(str);
+
+  for (size_t i = 0; i < len; i++)
+  {
+    int temp = 0;
+    temp = my_pow(10, len - i - 1);
+    if ((str[i] - '0') > iLimit / temp)
+    {
+      throw std::overflow_error("First parameter is out of range");
+    }
+    res += (str[i] - '0') * temp;
+  }
+  return res;
+}
+
+int my_pow(int a, int b)
+{
+  if (a == 0 || a == 1)
+  {
+    return a;
+  }
+  int res = 1;
+  for (int i = 0; i < b; i++)
+  {
+    if (res > std::numeric_limits<int>::max() / a)
+    {
+      throw std::overflow_error("First parameter is out of range");
+    }
+    res *= a;
+  }
+  return res;
+}
+
+size_t cnt_loc_min(int * arr, size_t x, size_t y){
+
+  size_t ans_2 = 0;
+  size_t total = x * y;
+
   for (size_t i = y + 1; i < total - y; ++i)
   {
     try
@@ -86,7 +181,14 @@ int main(int argc, char **argv)
     }
   }
 
+  return ans_2;
+}
+
+size_t num_col_lsr(int * arr, size_t x, size_t y)
+{
   int ans_11 = 0;
+  size_t total = x * y;
+  int max_length = 0;
 
   int mas[y][2];
   for (size_t i = 0; i < y; ++i)
@@ -109,28 +211,37 @@ int main(int argc, char **argv)
         ++mas[j][1];
       } else
       {
-        if(mas[j][1] > ans_11)
+        if(mas[j][1] > max_length)
         {
-          ans_11 = mas[j][0];
+          max_length = mas[j][1];
         }
         mas[j][1] = 1;
       }
     }
   }
 
+  
+
   for (size_t i = 0; i < y; ++i)
   {
-    if (ans_11 < mas[i][1])
+    if (max_length < mas[i][1])
     {
       ans_11 = mas[i][0];
+      max_length = mas[i][1];
     }
   }
+  
+  return ans_11;
+}
 
-  std::ofstream output;
-  output.open(argv[3]);
-
-  output << "Answer for var_2: " << ans_2 << "\n";
-  output << "Answer for var_11: " << ans_11 << "\n";
-
-  free(arr);
+void input_file(std::istream &in, int *m, size_t lng)
+{
+  for (size_t i = 0; i < lng; i++)
+  {
+    in >> m[i];
+    if (in.fail())
+    {
+      throw std::logic_error("Couldn't read the matrix");
+    }
+  }
 }
