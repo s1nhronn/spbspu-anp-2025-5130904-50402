88c723bc3f5daa4ff191f7f5c6175c631af8e212
khalii21 <miratkhalikov007@gmail.com>
2025-11-15 04:26:07 +0500

    input + output added

--- khalikov.mirat/P3/main.cpp
+++ khalikov.mirat/P3/main.cpp
@@ -1,2 +1,28 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+
+void output(std::ostream & out, const int * a, size_t n, size_t m)
+{
+  for (size_t i = 0; i < n; ++i)
+  {
+    for (size_t j = 0; j < m; ++j)
+    {
+       out << a[i*m + j] << " ";
+    }
+    std::cout << "\n";
+  }
+}
+
+void input(std::ostream & in, const int * a, size_t n, size_t m)
+{
+  for (size_t i = 0; i < n*m; ++i)
+  {
+    in >> a[i];
+  }
+}
+
+int main(int argc, char ** argv)
+{
+  std::ifstream input(argv[2]);
+  std::ofstream output(argv[3]);
+}
