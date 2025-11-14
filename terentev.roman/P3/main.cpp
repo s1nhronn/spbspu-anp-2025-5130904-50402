5c6fc3227b32258da7f509e721421cba2569db44
Omar702140 <rnmp4801@gmail.com>
2025-11-14 20:58:28 +0300

    Nachal massivi vorkati

--- terentev.roman/P3/main.cpp
+++ terentev.roman/P3/main.cpp
@@ -7,6 +7,7 @@ bool anum(char * a);
 int main(int argc, char ** argv)
 {
     std::ifstream input(argv[2]);
+    std::ofstream output(argv[3]);
     if (argc < 4)
     {
         std::cerr << "Not enough arguments\n";
@@ -17,22 +18,41 @@ int main(int argc, char ** argv)
         std::cerr << "Too many arguments\n";
         return 1;
     }
-    if (anum (argv[1])) {
+    if (!anum (argv[1])) {
         std::cerr << "First parameter is not a number\n";
         return 1;
     }
-    if (std::stoi(std::string(argv[1]))>2 || std::stoi(std::string(argv[1])) < 1) {
+    if (std::stoi(argv[1])>2 || std::stoi(argv[1]) < 1) {
         std::cerr << "First parameter is out of range\n";
+        return 1;
     }
-    std::ofstream output(argv[3]);
+    int n = 0, m = 0;
+    input >> n >> m;
+    if (!input || n < 0 || m < 0 || (n*m)>10000) {
+        std::cerr << "Array cannot exist\n";
+        return 2;
+    }
+    if (std::stoi(argv[1]) == 1) {
+        int a[10000] = {};
+
+    }
+    else {
+        int** a = (int**)malloc(n * sizeof(int*));
+        if (!a) {
+            std::cerr << "bad_alloc\n";
+            return 3;
+        }
+
+    }
+
     std::cout << "\n";
 }
 
 bool anum(char * a) {
     for (size_t i = 0; i < std::strlen(a); ++i) {
         if (!std::isdigit(static_cast<unsigned char>(a[i]))) {
-            return 1;
+            return 0;
         }
     }
-    return 0;
+    return 1;
 }
