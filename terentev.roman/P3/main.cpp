2b573e03cdf764dfeafaf5478f584af8fe7bc473
Omar702140 <rnmp4801@gmail.com>
2025-11-14 23:32:19 +0300

    +min +max

--- terentev.roman/P3/main.cpp
+++ terentev.roman/P3/main.cpp
@@ -6,8 +6,6 @@ bool anum(char * a);
 
 int main(int argc, char ** argv)
 {
-  std::ifstream input(argv[2]);
-  std::ofstream output(argv[3]);
   if (argc < 4)
   {
     std::cerr << "Not enough arguments\n";
@@ -26,9 +24,10 @@ int main(int argc, char ** argv)
     std::cerr << "First parameter is out of range\n";
     return 1;
   }
+  std::ifstream input(argv[2]);
+  std::ofstream output(argv[3]);
 
   int n = 0, m = 0;
-  input >> n >> m;
   if (!input || !(input >> n >> m) || n < 0 || m < 0 || (n*m)>10000) {
     std::cerr << "Array cannot exist\n";
     return 2;
@@ -39,21 +38,68 @@ int main(int argc, char ** argv)
       input >> a[i];
       if (!input) {
         std::cerr << "failed to read array element\n";
-        free(a);
         return 2;
       }
     }
+    size_t ix = m + 1;
+    int min = a[m + 1];
+    int max = a[m + 1];
+    while (ix < n * m - 1 - m) {
+      if (ix % m == 0) {
+        ix = ix + 1;
+      }
+      if (ix % m == m - 1) {
+        ix = ix + 2;
+      }
+      else {
+        if (a[ix] < a[ix - m - 1] && a[ix] < a[ix - m] && a[ix] < a[ix - m + 1] && a[ix] < a[ix - 1] && a[ix] < a[ix + 1] && a[ix] < a[ix + m - 1] && a[ix] < a[ix + m] && a[ix] < a[ix + m + 1]) {
+          min = a[ix];
+        }
+        if (a[ix] > a[ix - m - 1] && a[ix] > a[ix - m] && a[ix] > a[ix - m + 1] && a[ix] > a[ix - 1] && a[ix] > a[ix + 1] && a[ix] > a[ix + m - 1] && a[ix] > a[ix + m] && a[ix] > a[ix + m + 1]) {
+          max = a[ix];
+        }
+        ix = ix + 1;
+      }
+    }
+
   }
+
   else {
-    int** a = (int**)malloc(n * sizeof(int*));
+    int* a = (int*)malloc(n * sizeof(int*));
     if (!a) {
       std::cerr << "bad_alloc\n";
       return 3;
     }
+    for (size_t i = 0; i < n; ++i) {
+      input >> a[i];
+      if (!input) {
+        std::cerr << "failed to read array element\n";
+        free(a);
+        return 2;
+      }
+    }
+    size_t ix = m + 1;
+    int min = a[m + 1];
+    int max = a[m + 1];
+    while (ix < n * m - 1 - m) {
+      if (ix % m == 0) {
+        ix = ix + 1;
+      }
+      if (ix % m == m - 1) {
+        ix = ix + 2;
+      }
+      else {
+        if (a[ix] < a[ix - m - 1] && a[ix] < a[ix - m] && a[ix] < a[ix - m + 1] && a[ix] < a[ix - 1] && a[ix] < a[ix + 1] && a[ix] < a[ix + m - 1] && a[ix] < a[ix + m] && a[ix] < a[ix + m + 1]) {
+          min = a[ix];
+        }
+        if (a[ix] > a[ix - m - 1] && a[ix] > a[ix - m] && a[ix] > a[ix - m + 1] && a[ix] > a[ix - 1] && a[ix] > a[ix + 1] && a[ix] > a[ix + m - 1] && a[ix] > a[ix + m] && a[ix] > a[ix + m + 1]) {
+          max = a[ix];
+        }
+        ix = ix + 1;
+      }
+    }
 
   }
-
-  std::cout << "\n";
 }
 
 bool anum(char * a) {
@@ -61,5 +107,6 @@ bool anum(char * a) {
     if (!std::isdigit(static_cast<unsigned char>(a[i]))) {
       return 0;
     }
+  }
   return 1;
 }
