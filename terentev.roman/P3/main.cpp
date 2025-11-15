116cc3b961a7d03dcbd9ec6dbc2c68ba2670a755
Omar702140 <rnmp4801@gmail.com>
2025-11-15 17:41:55 +0300

    { fix +namespace

--- terentev.roman/P3/main.cpp
+++ terentev.roman/P3/main.cpp
@@ -2,8 +2,11 @@
 #include <fstream>
 #include <string>
 #include <cstring>
+namespace terentev{
 bool anum(char * a);
-int main(int argc, char** argv) {
+}
+int main(int argc, char** argv)
+{
   if (argc < 4)
   {
     std::cerr << "Not enough arguments\n";
@@ -14,11 +17,13 @@ int main(int argc, char** argv) {
     std::cerr << "Too many arguments\n";
     return 1;
   }
-  if (!anum(argv[1])) {
+  if (!std::anum(argv[1]))
+  {
     std::cerr << "First parameter is not a number\n";
     return 1;
   }
-  if (std::stoi(argv[1]) > 2 || std::stoi(argv[1]) < 1) {
+  if (std::stoi(argv[1]) > 2 || std::stoi(argv[1]) < 1)
+  {
     std::cerr << "First parameter is out of range\n";
     return 1;
   }
@@ -26,30 +31,37 @@ int main(int argc, char** argv) {
   std::ofstream output(argv[3]);
   size_t n = 0, m = 0;
   if (!input || !(input >> n >> m) ||
-    n == 1 || m == 1 || (n * m)>10000) {
+    n == 1 || m == 1 || (n * m)>10000)
+  {
     std::cerr << "Array cannot exist\n";
     return 2;
   }
-  if (n == 0 && m == 0) {
+  if (n == 0 && m == 0)
+  {
     output << "0\n";
     return 0;
   }
-  if (n == 2 || m == 2) {
+  if (n == 2 || m == 2)
+  {
     output << "0\n";
     return 0;
   }
-  if (std::stoi(argv[1]) == 1) {
+  if (std::stoi(argv[1]) == 1)
+  {
     int a[10000] = {};
     size_t count = 0;
-    for (size_t i = 0; i < n * m; ++i) {
+    for (size_t i = 0; i < n * m; ++i)
+      {
       input >> a[i];
       count = count + 1;
-      if (!input) {
+      if (!input)
+      {
         std::cerr << "Failed to read array element\n";
         return 2;
       }
     }
-    if (count != n * m) {
+    if (count != n * m)
+    {
       std::cerr << " Invalid number of array elements\n";
       return 2;
     }
@@ -58,14 +70,18 @@ int main(int argc, char** argv) {
     size_t kolmax = 0;
     int min = a[m + 1];
     int max = a[m + 1];
-    while (ix < n * m - 1 - m) {
-      if (ix % m == m - 1) {
+    while (ix < n * m - 1 - m)
+    {
+      if (ix % m == m - 1)
+      {
         ix = ix + 2;
       }
-      if (ix % m == 0) {
+      if (ix % m == 0)
+      {
         ix = ix + 1;
       }
-      else {
+      else
+      {
         if (a[ix] < a[ix - m - 1] && a[ix] < a[ix - m] && a[ix] < a[ix - m + 1] &&
           a[ix] < a[ix - 1] && a[ix] < a[ix + 1] && a[ix] < a[ix + m - 1] &&
           a[ix] < a[ix + m] && a[ix] < a[ix + m + 1]) {
@@ -79,33 +95,41 @@ int main(int argc, char** argv) {
         ix = ix + 1;
       }
     }
-    for (size_t p = m; p < n * m - m; ++p) {
-      if (a[p] == min && p % m != 0 && p % m != m - 1) {
+    for (size_t p = m; p < n * m - m; ++p)
+    {
+      if (a[p] == min && p % m != 0 && p % m != m - 1)
+      {
         kolmin = kolmin + 1;
       }
-      if (a[p] == max && p % m != 0 && p % m != m - 1) {
+      if (a[p] == max && p % m != 0 && p % m != m - 1)
+      {
         kolmax = kolmax + 1;
       }
     }
     output << kolmin << "\n" << kolmax << "\n";
   }
-  else {
+  else
+  {
     int* a = static_cast<int*>(malloc(n * m * sizeof(int)));
-    if (!a) {
+    if (!a)
+    {
       std::cerr << "bad_alloc\n";
       return 3;
     }
     size_t count = 0;
-    for (size_t i = 0; i < n * m; ++i) {
+    for (size_t i = 0; i < n * m; ++i)
+    {
       input >> a[i];
       count = count + 1;
-      if (!input) {
+      if (!input)
+      {
         std::cerr << "Failed to read array element\n";
         free(a);
         return 2;
       }
     }
-    if (count != n * m) {
+    if (count != n * m)
+      {
       std::cerr << " Invalid number of array elements\n";
       return 2;
     }
@@ -114,32 +138,41 @@ int main(int argc, char** argv) {
     int max = a[m + 1];
     size_t kolmin = 0;
     size_t kolmax = 0;
-    while (ix < n * m - 1 - m) {
-      if (ix % m == 0) {
+    while (ix < n * m - 1 - m)
+    {
+      if (ix % m == 0)
+      {
         ix = ix + 1;
       }
-      if (ix % m == m - 1) {
+      if (ix % m == m - 1)
+      {
         ix = ix + 2;
       }
-      else {
+      else
+      {
         if (a[ix] < a[ix - m - 1] && a[ix] < a[ix - m] && a[ix] < a[ix - m + 1] &&
           a[ix] < a[ix - 1] && a[ix] < a[ix + 1] && a[ix] < a[ix + m - 1] &&
-          a[ix] < a[ix + m] && a[ix] < a[ix + m + 1]) {
+          a[ix] < a[ix + m] && a[ix] < a[ix + m + 1])
+        {
           min = a[ix];
         }
         if (a[ix] > a[ix - m - 1] && a[ix] > a[ix - m] && a[ix] > a[ix - m + 1] &&
           a[ix] > a[ix - 1] && a[ix] > a[ix + 1] && a[ix] > a[ix + m - 1] &&
-          a[ix] > a[ix + m] && a[ix] > a[ix + m + 1]) {
+          a[ix] > a[ix + m] && a[ix] > a[ix + m + 1])
+        {
           max = a[ix];
         }
         ix = ix + 1;
       }
     }
-    for (size_t p = m; p < n * m - m; ++p) {
-      if (a[p] == min && p % m != 0 && p % m != m - 1) {
+    for (size_t p = m; p < n * m - m; ++p)
+    {
+      if (a[p] == min && p % m != 0 && p % m != m - 1)
+      {
         kolmin = kolmin + 1;
       }
-      if (a[p] == max && p % m != 0 && p % m != m - 1) {
+      if (a[p] == max && p % m != 0 && p % m != m - 1)
+      {
         kolmax = kolmax + 1;
       }
     }
@@ -148,9 +181,12 @@ int main(int argc, char** argv) {
   }
 }
 
-bool anum(char* a) {
-  for (size_t i = 0; i < std::strlen(a); ++i) {
-    if (!std::isdigit(static_cast<unsigned char>(a[i]))) {
+terentev::bool anum(char* a)
+{
+  for (size_t i = 0; i < std::strlen(a); ++i)
+  {
+    if (!std::isdigit(static_cast<unsigned char>(a[i])))
+    {
       return 0;
     }
   }
