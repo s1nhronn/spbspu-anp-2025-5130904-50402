ba08c2897c2cd4fda8538ef365c2974deb2197f9
khalii21 <miratkhalikov007@gmail.com>
2025-11-15 05:47:00 +0500

    checked for work

--- khalikov.mirat/P3/main.cpp
+++ khalikov.mirat/P3/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 
-void output(std::ostream & out, const int * a, size_t n, size_t m)
+void output_mtx(std::ostream & out, const int * a, size_t n, size_t m)
 {
   for (size_t i = 0; i < n; ++i)
   {
@@ -9,11 +9,11 @@ void output(std::ostream & out, const int * a, size_t n, size_t m)
     {
        out << a[i*m + j] << " ";
     }
-    std::cout << "\n";
+    out << "\n";
   }
 }
 
-void input(std::ostream & in, const int * a, size_t n, size_t m)
+void input_mtx(std::istream & in, int * a, size_t n, size_t m)
 {
   for (size_t i = 0; i < n*m; ++i)
   {
@@ -21,8 +21,36 @@ void input(std::ostream & in, const int * a, size_t n, size_t m)
   }
 }
 
+size_t cols(std::istream & in)
+{
+  size_t m = 0;
+  in >> m;
+  return m;
+}
+
+size_t rows(std::istream & in)
+{
+  size_t n = 0;
+  in >> n;
+  return n;
+}
+
 int main(int argc, char ** argv)
 {
+  if (argc < 4)
+  {
+    std::cerr << "Not enough arguments" << "\n";
+    return 1;
+  }
   std::ifstream input(argv[2]);
+  size_t n, m;
+  n = rows(input);
+  m = cols(input);
+  int * a = new int[n*m];
+  input_mtx(input, a, n, m);
+  input.close();
   std::ofstream output(argv[3]);
+  output_mtx(output, a, n, m);
+  delete[] a;
+  return 0;
 }
