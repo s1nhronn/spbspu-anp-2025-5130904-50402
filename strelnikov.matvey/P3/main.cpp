cee460a94785cf30dde3e5cec479801d35d8d974
hihikomori <ghhhhhnika@gmail.com>
2025-11-18 15:51:07 +0300

    final fixes i guess

--- strelnikov.matvey/P3/main.cpp
+++ strelnikov.matvey/P3/main.cpp
@@ -20,9 +20,7 @@ namespace strelnikov {
   {
     for (size_t i = 0; i < r; ++i) {
       for (size_t j = 0; j < c; ++j) {
-        if (!(in >> a[i * c + j])) {
-          throw std::logic_error("Wrong format\n");
-        }
+        in >> a[i * c + j];
       }
     }
   }
@@ -113,74 +111,7 @@ namespace strelnikov {
     return cnt;
   }
 
-  void doall(int pr, const char* inf, const char* outf)
-  {
-    std::ifstream in(inf);
-    if (!in.is_open()) {
-      throw std::logic_error("Cannot open input file\n");
-    }
-
-    size_t r, c;
-    in >> r >> c;
-    if (in.fail()) {
-      throw std::logic_error("Bad rows and cols file input\n");
-    }
-    if ((r > 1000 || c > 1000) && pr == 1) {
-      throw std::logic_error("Matrix too large\n");
-    }
-
-    int* mtx = nullptr;
-
-    try {
-      if (pr == 1) {
-        static int static_mtx[1000 * 1000];
-
-        input(in, static_mtx, r, c);
-        mtx = static_mtx;
-        in.close();
-
-        std::ofstream out(outf);
-        if (!out.is_open()) {
-          throw std::logic_error("Cannot open output file");
-        }
-
-        size_t dig = cntNzrDig(mtx, r, c);
-        out << dig << '\n';
-        lftBotCnt(mtx, r, c);
-        for (size_t i = 0; i < r; ++i) {
-          for (size_t j = 0; j < c; ++j)
-            out << mtx[i * c + j] << ' ';
-          out << '\n';
-        }
-        out.close();
-        return;
-      } else {
-        try {
-          mtx = new int[r * c];
-        } catch (std::bad_alloc& e) {
-          throw;
-        }
-        input(in, mtx, r, c);
-        in.close();
-
-        std::ofstream out(outf);
-        if (!out.is_open()) {
-          throw std::logic_error("Cannot open output file");
-        }
-        size_t dig = cntNzrDig(mtx, r, c);
-        lftBotCnt(mtx, r, c);
-        output(out, mtx, r, c, dig);
-        rm(mtx, r);
-      }
-    } catch (const std::exception& e) {
-      if (pr == 2) {
-        rm(mtx, r);
-      }
-      throw;
-    }
-  }
-
-} // namespace strelnikov
+}
 int main(int argc, char* argv[])
 {
   if (argc != 4) {
@@ -197,10 +128,64 @@ int main(int argc, char* argv[])
   }
   int pr = std::stoi(argv[1]);
 
-  try {
-    strelnikov::doall(pr, argv[2], argv[3]);
-  } catch (const std::exception& e) {
-    std::cerr << e.what();
+  std::ifstream in(argv[2]);
+  if (!in.is_open()) {
+    std::cerr << "Cannot open input file\n";
     return 2;
+  }
+
+  size_t r, c;
+  in >> r >> c;
+  if (!in) {
+    std::cerr << "Bad rows and cols file input\n";
+    return 2;
+  }
+  if ((r > 1000 || c > 1000) && pr == 1) {
+    std::cerr << "Matrix too large\n";
+    return 2;
+  }
+
+  int* mtx = nullptr;
+  try {
+    if (pr == 1) {
+      static int static_mtx[1000 * 1000];
+      strelnikov::input(in, static_mtx, r, c);
+      if (!in) {
+        std::cerr << "Bad input\n";
+        return 2;
+      }
+      mtx = static_mtx;
+      in.close();
+    } else {
+      mtx = new int[r * c];
+      strelnikov::input(in, mtx, r, c);
+      if (!in) {
+        std::cerr << "Bad input\n";
+        if (pr == 2) {
+          delete[] mtx;
+        }
+        return 2;
+      }
+      in.close();
+    }
+  } catch (const std::exception& e) {
+    if (pr == 2) {
+      delete[] mtx;
+    }
+    return 2;
+  }
+  std::ofstream out(argv[3]);
+  if (!out.is_open()) {
+    std::cerr << "Cannot open output file\n";
+    if (pr == 2) {
+      delete[] mtx;
+    }
+    return 2;
+  }
+  size_t dig = strelnikov::cntNzrDig(mtx, r, c);
+  strelnikov::lftBotCnt(mtx, r, c);
+  strelnikov::output(out, mtx, r, c, dig);
+  if (pr == 2) {
+    delete[] mtx;
   }
 }
