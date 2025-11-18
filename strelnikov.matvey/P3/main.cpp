ede5e7b2d8b1c9731534f98e059b979815318d7e
hihikomori <ghhhhhnika@gmail.com>
2025-11-18 14:31:09 +0300

    some minor fixes

--- strelnikov.matvey/P3/main.cpp
+++ strelnikov.matvey/P3/main.cpp
@@ -8,15 +8,33 @@ namespace strelnikov {
     delete[] a;
   }
 
+  bool prValidation(char* a)
+  {
+    if (std::string(a) != "1" && std::string(a) != "2") {
+      return false;
+    }
+    return true;
+  }
+
   void input(std::ifstream& in, int* a, size_t r, size_t c)
   {
     for (size_t i = 0; i < r; ++i) {
       for (size_t j = 0; j < c; ++j) {
-        in >> a[i * c + j];
-        if (in.fail()) {
-          throw std::logic_error("Bad input\n");
+        if (!(in >> a[i * c + j])) {
+          throw std::logic_error("Wrong format\n");
         }
       }
+    }
+  }
+
+  void output(std::ofstream& out, int* a, size_t r, size_t c, int dig)
+  {
+    out << dig << '\n';
+    for (size_t i = 0; i < r; ++i) {
+      for (size_t j = 0; j < c; ++j) {
+        out << a[i * c + j] << ' ';
+      }
+      out << '\n';
     }
   }
 
@@ -107,15 +125,15 @@ namespace strelnikov {
     if (in.fail()) {
       throw std::logic_error("Bad rows and cols file input\n");
     }
+    if ((r > 1000 || c > 1000) && pr == 1) {
+      throw std::logic_error("Matrix too large\n");
+    }
 
     int* mtx = nullptr;
 
     try {
       if (pr == 1) {
         static int static_mtx[1000 * 1000];
-        if (r > 1000 || c > 1000) {
-          throw std::logic_error("Matrix too large\n");
-        }
 
         input(in, static_mtx, r, c);
         mtx = static_mtx;
@@ -150,15 +168,8 @@ namespace strelnikov {
           throw std::logic_error("Cannot open output file");
         }
         size_t dig = cntNzrDig(mtx, r, c);
-        out << dig << '\n';
         lftBotCnt(mtx, r, c);
-        for (size_t i = 0; i < r; ++i) {
-          for (size_t j = 0; j < c; ++j) {
-            out << mtx[i * c + j] << ' ';
-          }
-          out << '\n';
-        }
-        out.close();
+        output(out, mtx, r, c, dig);
         rm(mtx, r);
       }
     } catch (const std::exception& e) {
@@ -170,40 +181,26 @@ namespace strelnikov {
   }
 
 } // namespace strelnikov
-int main(int argc, char const* argv[])
+int main(int argc, char* argv[])
 {
-  int ex_code = 0;
   if (argc != 4) {
     if (argc > 4) {
+      std::cerr << "Too many arguments\n";
     } else {
       std::cerr << "Too few arguments\n";
     }
-    ex_code = 1;
-  } else {
-    int pr;
-    try {
-      pr = std::stoi(argv[1]);
-    } catch (const std::invalid_argument& e) {
-      std::cerr << "First argument is not a number\n";
-      ex_code = 1;
-    } catch (const std::out_of_range& e) {
-      std::cerr << "First argument is out of range\n";
-      ex_code = 1;
-    }
-    if (ex_code == 0) {
-      if (pr != 1 && pr != 2) {
-        std::cerr << "First argument is out of range\n";
-        ex_code = 1;
-      }
-    }
-    if (ex_code == 0) {
-      try {
-        strelnikov::doall(pr, argv[2], argv[3]);
-      } catch (const std::exception& e) {
-        std::cerr << e.what();
-        ex_code = 2;
-      }
-    }
+    return 1;
   }
-  return ex_code;
+  if (!strelnikov::prValidation(argv[1])) {
+    std::cerr << "First argument must be a number(1 or 2)\n";
+    return 1;
+  }
+  int pr = std::stoi(argv[1]);
+
+  try {
+    strelnikov::doall(pr, argv[2], argv[3]);
+  } catch (const std::exception& e) {
+    std::cerr << e.what();
+    return 2;
+  }
 }
