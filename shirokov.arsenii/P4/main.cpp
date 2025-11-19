473e6390d670339938b2dde0535939c7c5887c9a
s1nhronn <silekatik@yandex.ru>
2025-11-19 17:17:13 +0300

    Added function declarators

--- shirokov.arsenii/P4/main.cpp
+++ shirokov.arsenii/P4/main.cpp
@@ -1,3 +1,13 @@
+#include <iostream>
+
+namespace shirokov
+{
+  char *getline(std::istream &in, size_t &s);
+  void expand(char *string, size_t &lng);
+  char *shr_sym(char *string, size_t lng);
+  char *uni_two(char *string, size_t lng);
+} // namespace shirokov
+
 int main()
 {
 }
