a5797a192b34963dbaeb1af1d8aa597bc22b8e3f
Stave143 <staveplay@mail.ru>
2025-11-20 22:50:57 +0300

    delete space before args

--- permyakov.sergey/P3/main.cpp
+++ permyakov.sergey/P3/main.cpp
@@ -4,15 +4,15 @@
 
 namespace permyakov
 {
-  void movePointToEnd (int * arr, size_t & ij, size_t & cnt, size_t end,
+  void movePointToEnd(int * arr, size_t & ij, size_t & cnt, size_t end,
   size_t & i, size_t & j, size_t m, bool isCntInc, bool isIncr);
-  void lftTopClk (int * arr1, int * arr, size_t n, size_t m);
-  void lftBotCnt (int * arr2, int * arr, size_t n, size_t m);
-  std::ifstream & arrInFromFile (std::ifstream & in, int * arr, size_t n, size_t m);
-  std::ofstream & arrOutInFile (std::ofstream & out, int * arr, size_t n, size_t m);
+  void lftTopClk(int * arr1, int * arr, size_t n, size_t m);
+  void lftBotCnt(int * arr2, int * arr, size_t n, size_t m);
+  std::ifstream & arrInFromFile(std::ifstream & in, int * arr, size_t n, size_t m);
+  std::ofstream & arrOutInFile(std::ofstream & out, int * arr, size_t n, size_t m);
 }
 
-int main (int argc, char ** argv)
+int main(int argc, char ** argv)
 {
   namespace per = permyakov;
   if (argc < 4) {
@@ -103,7 +103,7 @@ int main (int argc, char ** argv)
   free(arr2);
 }
 
-void permyakov::movePointToEnd (int * arr, size_t & ij, size_t & cnt, size_t end,
+void permyakov::movePointToEnd(int * arr, size_t & ij, size_t & cnt, size_t end,
 size_t & i, size_t & j, size_t m, bool isCntInc, bool isIncr)
 {
   if (isIncr) {
@@ -121,7 +121,7 @@ size_t & i, size_t & j, size_t m, bool isCntInc, bool isIncr)
   }
 }
 
-void permyakov::lftTopClk (int * arr1, int * arr, size_t n, size_t m)
+void permyakov::lftTopClk(int * arr1, int * arr, size_t n, size_t m)
 {
   if (n * m == 0) {
     return;
@@ -147,7 +147,7 @@ void permyakov::lftTopClk (int * arr1, int * arr, size_t n, size_t m)
   }
 }
 
-void permyakov::lftBotCnt (int * arr2, int * arr, size_t n, size_t m)
+void permyakov::lftBotCnt(int * arr2, int * arr, size_t n, size_t m)
 {
   if (n * m == 0) {
     return;
@@ -173,7 +173,7 @@ void permyakov::lftBotCnt (int * arr2, int * arr, size_t n, size_t m)
   }
 }
 
-std::ifstream & permyakov::arrInFromFile (std::ifstream & in, int * arr, size_t n, size_t m)
+std::ifstream & permyakov::arrInFromFile(std::ifstream & in, int * arr, size_t n, size_t m)
 {
   size_t s = 0;
   int h = 0;
@@ -187,7 +187,7 @@ std::ifstream & permyakov::arrInFromFile (std::ifstream & in, int * arr, size_t 
   return in;
 }
 
-std::ofstream & permyakov::arrOutInFile (std::ofstream & out, int * arr, size_t n, size_t m)
+std::ofstream & permyakov::arrOutInFile(std::ofstream & out, int * arr, size_t n, size_t m)
 {
   out << n << ' ' << m;
   for (size_t i = 0; i < n * m; ++i) {
