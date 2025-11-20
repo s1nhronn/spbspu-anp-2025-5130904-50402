a34ab2eb5590ee76efe9bc069991b5d2886b97e3
lavrentev.makar <makar.lavrentev@yandex.ru>
2025-11-20 18:22:36 +0300

    Fixed CG, constant, namespace and functions

--- lavrentev.makar/P3/main.cpp
+++ lavrentev.makar/P3/main.cpp
@@ -4,46 +4,42 @@
 #include <stdexcept>
 #include <cstring>
 #include <limits>
+#include <cstdlib>
 
-const size_t STATIC_SIZE = 10000;
-
-namespace pThrFunc
+namespace lavrentev
 {
   size_t cntLocMin(int* arr, size_t x, size_t y);
   size_t numColLsr(int* arr, size_t x, size_t y);
-  int inputFile(std::istream& in, int* m, size_t lng);
+  std::istream &inputFile(std::istream& in, int* m, size_t lng);
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
+  const size_t staticSize = 10000;
+
   if (argc < 4)
   {
-    std::cerr << "Not enough arguments" << "\n";
+    std::cerr << "Not enough arguments" << '\n';
     return 1;
   }
   if (argc > 4)
   {
-    std::cerr << "Too many arguments" << "\n";
+    std::cerr << "Too many arguments" << '\n';
     return 1;
   }
-  if (!isdigit(*argv[1]))
-  {
-    std::cerr << "First parameter is not a number" << "\n";
-    return 1;
-  }
+
   int num = 0;
-  try
+  char* endPtr;
+
+  num = strtol(argv[1], &endPtr, 10);
+  if (*endPtr != '\0')
   {
-    num = std::stoi(argv[1]);
-  }
-  catch (const std::overflow_error &e)
-  {
-    std::cerr << e.what() << '\n';
+    std::cerr << "First parameter is not a number";
     return 1;
   }
   if (num != 1 && num != 2)
   {
-    std::cerr << "First parameter is out of range" << "\n";
+    std::cerr << "First parameter is out of range" << '\n';
     return 1;
   }
 
@@ -51,7 +47,7 @@ int main(int argc, char **argv)
 
   if (!input.is_open())
   {
-    std::cerr << "Uncorrect file" << "\n";
+    std::cerr << "Uncorrect file" << '\n';
     return 2;
   }
 
@@ -59,34 +55,31 @@ int main(int argc, char **argv)
 
   if (!(input >> x >> y))
   {
-    std::cerr << "Uncorrect file" << "\n";
+    std::cerr << "Uncorrect file" << '\n';
     return 2;
   }
 
   size_t total = x * y;
-
-  int arr2[STATIC_SIZE] = {};
+  int arr2 [staticSize];
   int* arr = nullptr;
+  int* matrix = nullptr;
 
-  int *matrix = nullptr;
+  if (num == 1)
+  {
+    matrix = arr2;
+  }
+  else
+  {
+    int total_for_memory = x * y;
+    arr = reinterpret_cast< int* >(malloc(total_for_memory * sizeof(int)));
+    matrix = arr;
+  }
+
   try
   {
-    if (num == 1)
+    if (lavrentev::inputFile(input, matrix, total).fail())
     {
-      matrix = arr2;
-    }
-    else
-    {
-      int total_for_memory = static_cast<int>(x * y);
-      arr = static_cast <int*> (malloc(total_for_memory * sizeof(int)));
-      matrix = arr;
-    }
-    int k = pThrFunc::inputFile(input, matrix, total);
-    if (k == 2)
-    {
-      std::cerr << "Couldn't read the matrix" << "\n";
-      free(arr);
-      return 2;
+      throw std::logic_error("Couldn't read the matrix");
     }
   }
   catch (const std::logic_error &e)
@@ -96,20 +89,20 @@ int main(int argc, char **argv)
     return 2;
   }
 
-  size_t ans2 = pThrFunc::cntLocMin(matrix, x, y);
-  size_t ans11 = pThrFunc::numColLsr(matrix, x, y);
+  size_t ans2 = lavrentev::cntLocMin(matrix, x, y);
+  size_t ans11 = lavrentev::numColLsr(matrix, x, y);
 
   std::ofstream output(argv[3]);
 
-  output << "Answer for var_2: " << ans2 << "\n";
-  output << "Answer for var_11: " << ans11 << "\n";
+  output << "Answer for var_2: " << ans2 << '\n';
+  output << "Answer for var_11: " << ans11 << '\n';
 
   free(arr);
 }
 
-size_t pThrFunc::cntLocMin(int* arr, size_t x, size_t y){
-
-  size_t ans_2 = 0;
+size_t lavrentev::cntLocMin (int* arr, size_t x, size_t y)
+{
+  size_t ans2 = 0;
   size_t total = x * y;
 
   for (size_t i = y + 1; i < total - y; ++i)
@@ -122,21 +115,21 @@ size_t pThrFunc::cntLocMin(int* arr, size_t x, size_t y){
       b1 = b1 && (arr[i] < arr[i + y - 1]) && (arr[i] < arr[i - y + 1]);
       if (b1)
       {
-        ++ans_2;
+        ++ans2;
       }
     }
   }
 
-  return ans_2;
+  return ans2;
 }
 
-size_t pThrFunc::numColLsr(int* arr, size_t x, size_t y)
+size_t lavrentev::numColLsr(int* arr, size_t x, size_t y)
 {
-  int ans_11 = 0;
+  int ans11 = 0;
   size_t total = x * y;
   int max_length = 0;
-
   int mas[y][2];
+
   for (size_t i = 0; i < y; ++i)
   {
     mas[i][0] = -1;
@@ -172,23 +165,23 @@ size_t pThrFunc::numColLsr(int* arr, size_t x, size_t y)
   {
     if (max_length < mas[i][1])
     {
-      ans_11 = mas[i][0];
+      ans11 = mas[i][0];
       max_length = mas[i][1];
     }
   }
 
-  return ans_11;
+  return ans11;
 }
 
-int pThrFunc::inputFile(std::istream& in, int* m, size_t lng)
+std::istream &lavrentev::inputFile(std::istream& in, int* m, size_t lng)
 {
   for (size_t i = 0; i < lng; i++)
   {
     in >> m[i];
     if (in.fail())
     {
-      return 2;
+      return in;
     }
   }
-  return 0;
+  return in;
 }
