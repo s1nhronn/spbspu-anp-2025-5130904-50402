2593036a246c3483d2b098dfba261c99e60b0c4c
hihikomori <ghhhhhnika@gmail.com>
2025-11-22 21:55:17 +0300

    feat: add func to read input and create char array

---
+++ strelnikov.matvey/P4/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <cctype>
+
+void addSymb(char*& str, size_t& s, char ch)
+{
+  char* tmp = reinterpret_cast<char*>(malloc(s + 1));
+  if (tmp == nullptr) {
+    if (str) {
+      free(str);
+    }
+    throw std::bad_alloc();
+  }
+
+  for (size_t i = 0; i < s; ++i) {
+    tmp[i] = str[i];
+  }
+  if (str) {
+    free(str);
+  }
+  str = tmp;
+  str[s] = ch;
+  ++s;
+}
+
+char* getString(std::istream& in, size_t& s)
+{
+  bool isSkipWp = in.flags() & std::ios::skipws;
+  if (isSkipWp) {
+    in >> std::noskipws;
+  }
+  char* res = nullptr;
+  char ch;
+  while (in >> ch) {
+    if (ch == '\n') {
+      break;
+    }
+    try {
+      addSymb(res, s, ch);
+    } catch (const std::exception& e) {
+      if (res) {
+        free(res);
+      }
+      throw;
+    }
+  }
+
+  if (in.fail() && !in.eof()) {
+    throw std::logic_error("In fail");
+  }
+  try {
+    addSymb(res, s, '\0');
+  } catch (const std::exception& e) {
+    if (res) {
+      free(res);
+    }
+    throw;
+  }
+
+  return res;
+}
+
+int main()
+{
+  size_t s = 0;
+  char* str = nullptr;
+  try {
+    str = getString(std::cin, s);
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << '\n';
+    return 2;
+  }
+
+  for (size_t i = 0; i < s; ++i) {
+    std::cout << str[i];
+  }
+  std::cout << '\n' << s << '\n';
+  if (str) {
+    free(str);
+  }
+  return 0;
+}
