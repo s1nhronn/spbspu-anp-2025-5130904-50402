08c1a6abf2c80394eed438e494d69e39018247f1
Carrot_lord <lev.saldaev@gmai.com>
2025-11-25 19:20:32 +0300

    Add createCompactString function

--- saldaev.lev/P4/main.cpp
+++ saldaev.lev/P4/main.cpp
@@ -5,11 +5,11 @@
 namespace saldaev
 {
   const size_t block_size = 10;
-  
+
   size_t getline(std::istream &in, char *&data, const size_t block_size)
   {
     in >> std::noskipws;
-    data = static_cast<char *>(malloc(block_size + 1 * sizeof(char)));
+    data = static_cast< char * >(malloc(block_size + 1 * sizeof(char)));
     if (data == nullptr)
     {
       return 0;
@@ -23,7 +23,7 @@ namespace saldaev
       crnt_size++;
       if (crnt_size > crnt_msize)
       {
-        char *tmp = static_cast<char *>(malloc(crnt_size + block_size + 1 * sizeof(char)));
+        char *tmp = static_cast< char * >(malloc(crnt_size + block_size + 1 * sizeof(char)));
         if (tmp == nullptr)
         {
           return 0;
@@ -45,6 +45,37 @@ namespace saldaev
     }
     return 0;
   }
+
+  size_t countNewLength(char *arr, size_t length, char *&new_arr)
+  {
+    char previous = ' ';
+    char curent = ' ';
+    size_t leters = 0;
+    size_t spaces = 0;
+    for (size_t i = 0; i < length - 1; ++i)
+    {
+      curent = arr[i];
+      if (curent != ' ')
+      {
+        leters++;
+      }
+      else if(previous != ' ')
+      {
+        spaces++;
+      }
+      previous = curent;
+    }
+    if (leters && curent == ' ')
+    {
+      spaces--;
+    }
+    new_arr = static_cast< char * >(malloc((leters + spaces + 1) * sizeof(char)));
+    if (new_arr == nullptr)
+    {
+      return 0;
+    }
+    return (leters + spaces + 1);
+  }
 }
 
 int main()
@@ -55,5 +86,6 @@ int main()
   {
     std::cout << a[i];
   }
+  std::cout << '\n' << k << ' ' << saldaev::countNewLength(a, k, a) << '\n';
   free(a);
 }
