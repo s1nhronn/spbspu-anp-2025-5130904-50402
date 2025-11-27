229e64929dc0c1d7783ab48becc289265b8f3707
Sp0146001 <sp0146001@gmail.com>
2025-11-27 20:46:19 +0300

    Write input func + add bodies of tasks functions

--- petrov.sasha/P4/main.cpp
+++ petrov.sasha/P4/main.cpp
@@ -1,2 +1,52 @@
 #include <iostream>
-int main(){}
+#include <iomanip>
+#include <cctype>
+namespace petrov
+{
+  char* getLine(std::istream& input) {
+    size_t lenght = 0;
+    bool skipWs = in.flags() & std::ios:skipws;
+    if(skipWs) {
+      input >> std::noskipws;
+    }
+    char* resStr = nullptr;
+    char tmp = ' ';
+    while ( input >> tmp && tmp != '\n') {
+      char* tmpStr = static_cast< char* >(realloc(resStr, lenght+1));
+      if (tmpStr == nullptr) {
+        free(resStr);
+        throw std::bad_alloc("Bad allocate error\n");
+      }
+      resStr = tmpStr;
+      resStr[lenght++] = tmp;
+    }
+    if (input.fail() && !input.eof()) {
+      free(resStr);
+      throw std::logic_error("Invalid reading\n");
+    }
+    if (skipWs) {
+      input >> std::skipws;
+    }
+    resStr[lenght] = '\0';
+    return resStr;
+  }
+
+  char* doUncSym() {
+  
+  }
+
+  size_t doSeqSym() {
+  
+  }
+}
+
+int main()
+{
+  char* str = nullptr;
+  try {
+    str = petrov::getLine(std::cin);
+  } catch (const std::logic_error& e) {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
+}
