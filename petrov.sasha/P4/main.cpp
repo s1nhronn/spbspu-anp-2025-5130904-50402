2a08cc18c6836731e840f1ff34a067905cf623cb
Sp0146001 <sp0146001@gmail.com>
2025-11-27 21:49:16 +0300

    Add function of Unc-Sym & write addFunc for this var

--- petrov.sasha/P4/main.cpp
+++ petrov.sasha/P4/main.cpp
@@ -3,8 +3,7 @@
 #include <cctype>
 namespace petrov
 {
-  char* getLine(std::istream& input) {
-    size_t lenght = 0;
+  char* getLine(std::istream& input, size_t& lenght) {
     bool skipWs = input.flags() & std::ios::skipws;
     if(skipWs) {
       input >> std::noskipws;
@@ -30,9 +29,33 @@ namespace petrov
     resStr[lenght] = '\0';
     return resStr;
   }
-
-  char* doUncSym() {
-  
+  void duplicateForUncSym(const char* str1, const char* str2, char* result, size_t place) {
+    for(size_t i = 0; str1[i] != '\0'; ++i) {
+      bool flag1 = false;
+      for (size_t j = 0; str2[j] != '\0'; ++j) {
+        if (str1[i] == str2[j]) {
+          flag1 = true;
+          break;
+        }
+      }
+      if(!flag1) {
+        bool flag2 = false;
+        for (size_t k = 0; k < place; ++k) {
+          if (result[k] == str1[i]) {
+            flag2 = true;
+            break
+          }
+        if (!flag2) {
+          result[place++] = str1[i];
+        }
+      }
+    }
+  }
+  void doUncSym(const char* str1, const char* str2, char* result) {
+    size_t place = 0;
+    duplicateForUncSym(str1, str2, result, place);
+    duplicateForUncSym(str2, str1, result, place);
+    result[place] = '\0';
   }
 
   size_t doSeqSym() {
@@ -43,10 +66,28 @@ namespace petrov
 int main()
 {
   char* str = nullptr;
+  size_t len = 0;
   try {
-    str = petrov::getLine(std::cin);
+    str = petrov::getLine(std::cin, len);
   } catch (const std::logic_error& e) {
     std::cerr << e.what() << "\n";
     return 1;
+  } catch (const std::bad_alloc& e) {
+    std::cerr << "Bad alloc error\n";
+    return 1;
   }
+  const char* sec_str = "abc_ef";
+  char* ansUncSym = static_cast< char* >(malloc(len+7));
+  if (ansUncSym == nullptr) {
+    std::cerr << "Alloc failed\n";
+    free(str);
+    return 1;
+  }
+  doUncSym(str, sec_str, ansUncSym);
+  size_t ansSeqSym = doSeqSym();
+  std::cout << ansUncSym << "\n";
+  std::cout << ansSeqSym << "\n";
+  free(str);
+  free(ansUncSym);
+  return 0;
 }
