af3768e71e25fbdc83a4deca32f6bc97b49b4a24
Omar702140 <rnmp4801@gmail.com>
2025-11-29 19:40:50 +0300

    remake

--- terentev.roman/P3/main.cpp
+++ terentev.roman/P3/main.cpp
@@ -116,15 +116,77 @@ void terentev::minmax(const int* a, size_t n, size_t m, size_t& kolmin, size_t& 
     }
     else
     {
-      if (a[ix] < a[ix - m - 1] && a[ix] < a[ix - m] && a[ix] < a[ix - m + 1] &&
-        a[ix] < a[ix - 1] && a[ix] < a[ix + 1] && a[ix] < a[ix + m - 1] &&
-        a[ix] < a[ix + m] && a[ix] < a[ix + m + 1])
+      bool ifi = true;
+      if (a[ix] >= a[ix - m - 1])
+      {
+        ifi = false;
+      }
+      if (a[ix] >= a[ix - m])
+      {
+        ifi = false;
+      }
+      if (a[ix] >= a[ix - m + 1])
+      {
+        ifi = false;
+      }
+      if (a[ix] >= a[ix - 1])
+      {
+        ifi = false;
+      }
+      if (a[ix] >= a[ix + 1])
+      {
+        ifi = false;
+      }
+      if (a[ix] >= a[ix + m - 1])
+      {
+        ifi = false;
+      }
+      if (a[ix] >= a[ix + m])
+      {
+        ifi = false;
+      }
+      if (a[ix] >= a[ix + m + 1])
+      {
+        ifi = false;
+      }
+      if (ifi)
       {
         min = a[ix];
       }
-      if (a[ix] > a[ix - m - 1] && a[ix] > a[ix - m] && a[ix] > a[ix - m + 1] &&
-        a[ix] > a[ix - 1] && a[ix] > a[ix + 1] && a[ix] > a[ix + m - 1] &&
-        a[ix] > a[ix + m] && a[ix] > a[ix + m + 1])
+      ifi = true;
+      if (a[ix] <= a[ix - m - 1])
+      {
+        ifi = false;
+      }
+      if (a[ix] <= a[ix - m])
+      {
+        ifi = false;
+      }
+      if (a[ix] <= a[ix - m + 1])
+      {
+        ifi = false;
+      }
+      if (a[ix] <= a[ix - 1])
+      {
+        ifi = false;
+      }
+      if (a[ix] <= a[ix + 1])
+      {
+        ifi = false;
+      }
+      if (a[ix] <= a[ix + m - 1])
+      {
+        ifi = false;
+      }
+      if (a[ix] <= a[ix + m])
+      {
+        ifi = false;
+      }
+      if (a[ix] <= a[ix + m + 1])
+      {
+        ifi = false;
+      }
+      if (ifi)
       {
         max = a[ix];
       }
