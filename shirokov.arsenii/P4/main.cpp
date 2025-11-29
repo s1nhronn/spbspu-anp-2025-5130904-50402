a1dfdb17ce3f707f034e8d1089cdb175838516b5
s1nhronn <silekatik@yandex.ru>
2025-11-30 01:17:15 +0300

    Correction according to the requirements

--- shirokov.arsenii/P4/main.cpp
+++ shirokov.arsenii/P4/main.cpp
@@ -8,12 +8,13 @@
 namespace shirokov
 {
   size_t LATIN_ALPHABET_LENGTH = 26;
+  const int LETTER_DELTA = 32;
   const char LITERAL[] = "def ";
-  char *uniq(const char *str, size_t s, size_t &rsize);
+  char *uniq(char *res, const char *str, size_t &rsize);
   void expand(char **str, size_t size, size_t &capacity);
   char *getline(std::istream &in, size_t &s);
-  void SHR_SYM(const char *str, size_t s, char *res);
-  void UNI_TWO(const char *str1, size_t s1, const char *str2, size_t s2, char *res);
+  char *otherLatinLetters(const char *str, char *res, char *buffer);
+  void combineLines(const char *str1, size_t s1, const char *str2, size_t s2, char *res);
 }
 
 int main()
@@ -29,8 +30,7 @@ int main()
   }
 
   char *res1 = static_cast< char * >(malloc((shirokov::LATIN_ALPHABET_LENGTH + 1) * sizeof(char)));
-  char *res2 =
-      static_cast< char * >(malloc((std::strlen(shirokov::LITERAL) + s + 1) * sizeof(char)));
+  char *res2 = static_cast< char * >(malloc((std::strlen(shirokov::LITERAL) + s + 1) * sizeof(char)));
   if (res1 == nullptr || res2 == nullptr)
   {
     free(str);
@@ -39,9 +39,19 @@ int main()
     std::cerr << "Memory allocation error\n";
     return 1;
   }
-
-  shirokov::SHR_SYM(str, s, res1);
-  shirokov::UNI_TWO(str, s, shirokov::LITERAL, std::strlen(shirokov::LITERAL), res2);
+  res1[shirokov::LATIN_ALPHABET_LENGTH] = '\0';
+  res2[std::strlen(shirokov::LITERAL) + s] = '\0';
+  char *buffer = static_cast< char * >(malloc(sizeof(char) * s));
+  if (buffer == nullptr)
+  {
+    free(str);
+    free(res1);
+    free(res2);
+    std::cerr << "Memory allocation error\n";
+    return 1;
+  }
+  shirokov::otherLatinLetters(str, res1, buffer);
+  shirokov::combineLines(str, s, shirokov::LITERAL, std::strlen(shirokov::LITERAL), res2);
   if (res1 == nullptr || res2 == nullptr)
   {
     free(str);
@@ -67,24 +77,27 @@ char *shirokov::getline(std::istream &in, size_t &s)
   }
   size_t capacity = 1;
   s = 0;
-  char *str = static_cast< char * >(malloc(capacity * sizeof(char)));
+  char *str = reinterpret_cast< char * >(malloc(capacity * sizeof(char)));
   if (str == nullptr)
   {
     return nullptr;
   }
   while (in)
   {
-    shirokov::expand(&str, s, capacity);
+    if (s == capacity)
+    {
+      shirokov::expand(&str, s, capacity);
+    }
     if (str == nullptr)
     {
       return nullptr;
     }
     in >> str[s];
-    if (in.eof())
+    if (!in || str[s] == '\0')
     {
       break;
     }
-    s += 1;
+    s++;
   }
   if (in.bad() || s == 0)
   {
@@ -99,15 +112,10 @@ char *shirokov::getline(std::istream &in, size_t &s)
   return str;
 }
 
-void shirokov::SHR_SYM(const char *str, size_t s, char *res)
+char *shirokov::otherLatinLetters(const char *str, char *res, char *buffer)
 {
   size_t rsize = 0;
-  char *ustr = uniq(str, s, rsize);
-  if (ustr == nullptr)
-  {
-    res = nullptr;
-    return;
-  }
+  char *ustr = uniq(buffer, str, rsize);
   size_t pos = 0;
   for (char letter = 'a'; letter <= 'z'; ++letter)
   {
@@ -127,11 +135,13 @@ void shirokov::SHR_SYM(const char *str, size_t s, char *res)
   }
   res[pos] = '\0';
   free(ustr);
+  return res;
 }
 
-void shirokov::UNI_TWO(const char *str1, size_t s1, const char *str2, size_t s2, char *res)
+void shirokov::combineLines(const char *str1, size_t s1, const char *str2, size_t s2, char *res)
 {
   size_t minn = s1 < s2 ? s1 : s2;
+  res[s1 + s2] = '\0';
   for (size_t i = 0; i < minn; ++i)
   {
     res[2 * i] = str1[i];
@@ -141,27 +151,21 @@ void shirokov::UNI_TWO(const char *str1, size_t s1, const char *str2, size_t s2,
   {
     res[i] = (s1 > s2 ? str1[i - minn] : str2[i - minn]);
   }
-  res[s1 + s2] = '\0';
 }
 
-char *shirokov::uniq(const char *str, size_t s, size_t &rsize)
+char *shirokov::uniq(char *res, const char *str, size_t &rsize)
 {
   rsize = 0;
-  char *res = static_cast< char * >(malloc(sizeof(char) * s));
-  if (res == nullptr)
-  {
-    return nullptr;
-  }
-  for (size_t i = 0; i < s; ++i)
+  for (size_t i = 0; str[i] != '\0'; ++i)
   {
     bool in_res = false;
+    char temp = str[i];
+    if ('A' <= temp && temp <= 'Z')
+    {
+      temp += shirokov::LETTER_DELTA;
+    }
     for (size_t j = 0; j < rsize; ++j)
     {
-      char temp = str[i];
-      if ('A' <= temp && temp <= 'Z')
-      {
-        temp += 32;
-      }
       if (temp == res[j])
       {
         in_res = true;
@@ -183,20 +187,17 @@ char *shirokov::uniq(const char *str, size_t s, size_t &rsize)
 void shirokov::expand(char **str, size_t size, size_t &capacity)
 {
   char *temp_str = nullptr;
-  if (size == capacity)
+  temp_str = static_cast< char * >(malloc(capacity * 2));
+  if (temp_str == nullptr)
   {
-    capacity *= 2;
-    temp_str = static_cast< char * >(malloc(capacity));
-    if (temp_str == nullptr)
-    {
-      *str = nullptr;
-      return;
-    }
-    for (size_t i = 0; i < size; ++i)
-    {
-      temp_str[i] = (*str)[i];
-    }
-    free(*str);
-    *str = temp_str;
+    *str = nullptr;
+    return;
   }
+  for (size_t i = 0; i < size; ++i)
+  {
+    temp_str[i] = (*str)[i];
+  }
+  free(*str);
+  *str = temp_str;
+  capacity *= 2;
 }
