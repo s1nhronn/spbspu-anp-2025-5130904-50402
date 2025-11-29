deccc787b9acfdacdbe03de0d42e56a4948d05ee
Omar702140 <rnmp4801@gmail.com>
2025-11-29 16:14:49 +0300

    < >

--- terentev.roman/P3/main.cpp
+++ terentev.roman/P3/main.cpp
@@ -76,7 +76,7 @@ int main(int argc, char** argv)
   }
   else
   {
-    a = reinterpret_cast<int*>(malloc(n * m * sizeof(int)));
+    a = reinterpret_cast< int* >(malloc(n * m * sizeof(int)));
     if (!a)
     {
       std::cerr << "bad_alloc\n";
