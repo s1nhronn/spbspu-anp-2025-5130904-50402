365e75232859503193c816b844fb67b2d039d7c5
Carrot_lord <lev.saldaev@gmai.com>
2025-12-04 03:03:11 +0300

    finalize function logic changes

--- saldaev.lev/P4/main.cpp
+++ saldaev.lev/P4/main.cpp
@@ -6,10 +6,10 @@
 namespace saldaev
 {
   const size_t block_size = 10;
-  
-  char * getLine(std::istream &in, const size_t block_size)
+
+  char *getLine(std::istream &in, const size_t block_size)
   {
-    char * data = reinterpret_cast< char * >(malloc((block_size + 1) * sizeof(char)));
+    char *data = reinterpret_cast<char *>(malloc((block_size + 1) * sizeof(char)));
     if (data == nullptr)
     {
       return nullptr;
@@ -28,7 +28,7 @@ namespace saldaev
       size++;
       if (size > capacity)
       {
-        char *tmp = reinterpret_cast< char * >(malloc((capacity + block_size + 1) * sizeof(char)));
+        char *tmp = reinterpret_cast<char *>(malloc((capacity + block_size + 1) * sizeof(char)));
         if (tmp == nullptr)
         {
           free(data);
@@ -61,11 +61,11 @@ namespace saldaev
     return nullptr;
   }
 
-  char * createCompactArray1(const char *data)
+  char *createCompactArray1(const char *data)
   {
     if (data == nullptr)
     {
-        return nullptr;
+      return nullptr;
     }
     char prev = ' ';
     char crnt = ' ';
@@ -75,137 +75,131 @@ namespace saldaev
     crnt = data[i];
     while (crnt != '\0')
     {
-        if (crnt != ' ')
-        {
-            leters++;
-        }
-        else if (prev != ' ' && crnt == ' ')
-        {
-            spaces++;
-        }
-        prev = crnt;
-        i++;
-        crnt = data[i];
+      if (crnt != ' ')
+      {
+        leters++;
+      }
+      else if (prev != ' ' && crnt == ' ')
+      {
+        spaces++;
+      }
+      prev = crnt;
+      i++;
+      crnt = data[i];
     }
     if (leters && prev == ' ')
     {
-        spaces--;
+      spaces--;
     }
     char *new_array = reinterpret_cast<char *>(malloc((leters + spaces + 1) * sizeof(char)));
     if (new_array == nullptr)
     {
-        return nullptr;
+      return nullptr;
     }
     new_array[leters + spaces] = '\0';
     return new_array;
   }
 
-  size_t createCompactArray2(const char *data, const size_t length, char *&new_array)
+  char *createCompactArray2(const char *data)
   {
-    size_t new_length = length;
-    for (size_t i = 0; i < length; ++i)
+    size_t new_length = 1;
+    size_t i = 0;
+    char crnt = data[i];
+    while (crnt != '\0')
     {
-      if (std::isalpha(data[i]))
+      if (!std::isalpha(crnt))
       {
-        new_length--;
+        new_length++;
       }
+      i++;
+      crnt = data[i];
     }
-    new_array = reinterpret_cast< char * >(malloc(new_length * sizeof(char)));
+    char *new_array = reinterpret_cast<char *>(malloc(new_length * sizeof(char)));
     if (new_array == nullptr)
     {
-      return 0;
+      return nullptr;
     }
-    return new_length;
+    return new_array;
   }
 
-  size_t spcRmv(const char *arr, size_t length, char *&new_arr)
+  char * spcRmv(const char *data)
   {
-    size_t new_length = createCompactArray1(arr, length, new_arr);
-    if (new_length == 0)
+    char * new_arr = createCompactArray1(data);
+    if (new_arr == nullptr)
     {
       return 0;
     }
     size_t crnt_digit = 0;
-    char crnt_char = ' ';
-    char prev_char = ' ';
     size_t i = 0;
-    while (crnt_digit < new_length)
+    char crnt_char = data[i];
+    char prev_char = ' ';
+    while (crnt_char != '\0')
     {
-      crnt_char = arr[i];
       if (crnt_char != ' ' || prev_char != ' ')
       {
         new_arr[crnt_digit] = crnt_char;
         crnt_digit++;
       }
-      i++;
       prev_char = crnt_char;
+      i++;
+      crnt_char = data[i];
     }
-    return new_length;
+    return new_arr;
   }
-  size_t latRmv(char *arr, size_t length, char *&new_arr)
+  char * latRmv(char *data)
   {
-    size_t new_length = createCompactArray2(arr, length, new_arr);
-    if (new_length == 0)
+    char * new_arr = createCompactArray2(data);
+    if (new_arr == 0)
     {
       return 0;
     }
     size_t crnt_digit = 0;
-    char crnt_char = ' ';
     size_t i = 0;
-    while (crnt_digit < new_length)
+    char crnt_char = data[i];
+    while (crnt_char != '\0')
     {
-      crnt_char = arr[i];
       if (!isalpha(crnt_char))
       {
         new_arr[crnt_digit] = crnt_char;
         crnt_digit++;
       }
       i++;
+      crnt_char = data[i];
     }
-    return new_length;
+    return new_arr;
   }
 }
 
 int main()
 {
   char *line = nullptr;
-  size_t k = saldaev::getLine(std::cin, line, saldaev::block_size);
-  if (k == 0)
+  line = saldaev::getLine(std::cin, saldaev::block_size);
+  if (line == nullptr)
   {
     std::cerr << "Could not read the string\n";
-    free(line);
     return 1;
   }
 
   char *new_line = nullptr;
-  size_t nk = saldaev::spcRmv(line, k, new_line);
-  if (nk == 0)
+  new_line = saldaev::spcRmv(line);
+  if (new_line == nullptr)
   {
     std::cerr << "Could not convert the string\n";
     free(line);
     return 1;
   }
-  for (size_t i = 0; i < nk; ++i)
-  {
-    std::cout << new_line[i];
-  }
-  std::cout << "\n";
+  std::cout << new_line << "\n";
   free(new_line);
 
   new_line = nullptr;
-  nk = saldaev::latRmv(line, k, new_line);
-  if (nk == 0)
+  new_line = saldaev::latRmv(line);
+  if (new_line == nullptr)
   {
     std::cerr << "Could not convert the string\n";
     free(line);
     return 1;
   }
-  for (size_t i = 0; i < nk; ++i)
-  {
-    std::cout << new_line[i];
-  }
-  std::cout << "\n";
+  std::cout << new_line << "\n";
   free(line);
   free(new_line);
 }
-
