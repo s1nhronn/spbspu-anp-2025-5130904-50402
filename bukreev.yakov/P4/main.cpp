cf0aa0b1e42b96b4ba9b8b812472cc6b9019dfb7
BukreevYas <bukreev.yas@edu.spbstu.ru>
2025-12-05 06:30:15 -0500

    Fixed mistakes.

--- bukreev.yakov/P4/main.cpp
+++ bukreev.yakov/P4/main.cpp
@@ -7,9 +7,9 @@ namespace bukreev
   const size_t initialSize = 4;
 
   char* inputString();
-  void deleteString(const char* str);
+  void deleteString(char* str);
 
-  char* growString(const char* oldStr, size_t* capacity);
+  char* growString(char** oldStr, size_t& capacity);
 
   char* excsnd(const char* first, const char* second, char* resStr);
   char* latrmv(const char* str, char* resStr);
@@ -19,17 +19,14 @@ int main()
 {
   char* str = nullptr;
 
-  try
-  {
-    str = bukreev::inputString();
-  }
-  catch(const std::bad_alloc& e)
+  str = bukreev::inputString();
+  if (!str)
   {
     std::cerr << "Not enough memory for string input.\n";
     return 1;
   }
 
-  if (strlen(str) == 0)
+  if (std::strlen(str) == 0)
   {
     bukreev::deleteString(str);
     std::cerr << "Error: no input.\n";
@@ -37,7 +34,7 @@ int main()
   }
 
   char* res1 = nullptr, *res2 = nullptr;
-  res1 = reinterpret_cast<char*>(malloc((strlen(str) + 1) * sizeof(char)));
+  res1 = reinterpret_cast< char* >(malloc((std::strlen(str) + 1) * sizeof(char)));
   if (!res1)
   {
     std::cerr << "Not enough memory for EXC_SND.\n";
@@ -45,7 +42,7 @@ int main()
     return 1;
   }
 
-  res2 = reinterpret_cast<char*>(malloc((strlen(str) + 1) * sizeof(char)));
+  res2 = reinterpret_cast< char* >(malloc((std::strlen(str) + 1) * sizeof(char)));
   if (!res2)
   {
     std::cerr << "Not enough memory for LAT_RMV.\n";
@@ -54,13 +51,15 @@ int main()
     return 1;
   }
 
-  res1 = bukreev::excsnd(str, "abc", res1);
+  const char* stringToExclude = "abc";
+
+  res1 = bukreev::excsnd(str, stringToExclude, res1);
   res2 = bukreev::latrmv(str, res2);
 
   bukreev::deleteString(str);
 
-  std::cout << res1;
-  std::cout << res2;
+  std::cout << res1 << '\n';
+  std::cout << res2 << '\n';
 
   bukreev::deleteString(res1);
   bukreev::deleteString(res2);
@@ -70,20 +69,25 @@ char* bukreev::inputString()
 {
   size_t capacity = initialSize;
 
-  char* buffer = reinterpret_cast<char*>(malloc(initialSize * sizeof(char)));
+  char* buffer = reinterpret_cast< char* >(malloc(initialSize * sizeof(char)));
   if (!buffer)
   {
-    throw std::bad_alloc();
+    return nullptr;
   }
 
   std::cin >> std::noskipws;
 
   size_t i = 0;
-  while (true)
+  bool run = true;
+  while (run)
   {
     if (i >= capacity)
     {
-      buffer = growString(buffer, &capacity);
+      buffer = growString(&buffer, capacity);
+      if (!buffer)
+      {
+        return nullptr;
+      }
     }
 
     std::cin >> buffer[i];
@@ -95,46 +99,42 @@ char* bukreev::inputString()
 
     if (buffer[i] == '\n')
     {
-      i++;
-      if (i >= capacity)
-      {
-        buffer = growString(buffer, &capacity);
-      }
-
       buffer[i] = 0;
-      break;
+      run = false;
     }
 
     i++;
   }
 
+  std::cin >> std::skipws;
+
   return buffer;
 }
 
-void bukreev::deleteString(const char* str)
+void bukreev::deleteString(char* str)
 {
-  free(const_cast<char*>(str));
+  free(str);
 }
 
-char* bukreev::growString(const char* oldStr, size_t* capacity)
+char* bukreev::growString(char** oldStr, size_t& capacity)
 {
   const double growRatio = 1.5;
 
-  size_t oldCapacity = *capacity;
+  size_t oldCapacity = capacity;
   size_t newCapacity = oldCapacity * growRatio;
 
-  *capacity = newCapacity;
-
-  char* newStr = reinterpret_cast<char*>(malloc(newCapacity * sizeof(char)));
+  char* newStr = reinterpret_cast< char* >(malloc(newCapacity * sizeof(char)));
   if (!newStr)
   {
-    deleteString(oldStr);
-    throw std::bad_alloc();
+    deleteString(*oldStr);
+    return nullptr;
   }
 
-  std::strncpy(newStr, oldStr, oldCapacity);
+  capacity = newCapacity;
+  std::strncpy(newStr, *oldStr, oldCapacity);
 
-  deleteString(oldStr);
+  deleteString(*oldStr);
+  *oldStr = newStr;
 
   return newStr;
 }
@@ -143,12 +143,12 @@ char* bukreev::excsnd(const char* first, const char* second, char* resStr)
 {
   size_t resIndex = 0;
 
-  for (size_t i = 0; i < std::strlen(first); i++)
+  for (size_t i = 0; first[i] != 0; i++)
   {
     char c = first[i];
     bool exclude = false;
 
-    for (size_t j = 0; j < std::strlen(second); j++)
+    for (size_t j = 0; second[j] != 0; j++)
     {
       if (second[j] == c)
       {
@@ -173,7 +173,7 @@ char* bukreev::latrmv(const char* str, char* resStr)
 {
   size_t resIndex = 0;
 
-  for (size_t i = 0; i < std::strlen(str); i++)
+  for (size_t i = 0; str[i] != 0; i++)
   {
     if (!std::isalpha(str[i]))
     {
