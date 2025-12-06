d42dd583eb5a78ea8e60c27f91f4587b175941e4
voyadger01 <voyadgerkx@gmail.com>
2025-12-06 21:19:50 +0300

    preparing for lab

--- karpovich.dmitriy/P5/main.cpp
+++ karpovich.dmitriy/P5/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
+namespace karpovich
+{
+  struct point_t
+  {
+    double x, y;
+  };
+  struct rectangle_t
+  {
+    double width, height, pos;
+  };
+  struct Shape
+  {
+    double getArea();
+    double getFrameRect();
+    void move();
+    void scale();
+  };
+  struct Rectangle: Shape
+  {
+
+  };
+}
+
 int main()
 {
-  
+  namespace karp = karpovich;
 }
