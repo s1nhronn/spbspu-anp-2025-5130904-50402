7ed100954d99cd816a49c7d5d796e86951476ff3
voyadger01 <voyadgerkx@gmail.com>
2025-12-09 12:20:22 +0300

    declaration of rectangle

--- karpovich.dmitriy/P5/main.cpp
+++ karpovich.dmitriy/P5/main.cpp
@@ -12,14 +12,23 @@ namespace karpovich
   };
   struct Shape
   {
-    double getArea();
-    double getFrameRect();
-    void move();
-    void scale();
+    virtual double getArea() const = 0;
+    virtual rectangle_t getFrameRect() const = 0;
+    virtual void move(point_t p) = 0;
+    virtual void move(double dx, double dy) = 0;
+    virtual void scale(double k) = 0;
   };
   struct Rectangle: Shape
   {
-
+    Rectangle(double width, double height, point_t centr);
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(double dx, double dy) override;
+    void move(point_t p) override;
+    void scale(double k) override;
+    private:
+      double width_, height_;
+      point_t centr_;
   };
 }
 
