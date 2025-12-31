c6669ae9497e39df25ce6da8e0314e89a8ef6e37
Alastor20 <nik_d.007@mail.ru>
2025-12-31 16:55:40 +0300

    P5 shapes header

---
+++ dirko.nikita/P5/shapes.hpp
@@ -0,0 +1,49 @@
+#ifndef SHAPES
+#define SHAPES
+#include <cmath>
+#include "shapesUtil.hpp"
+namespace dirko
+{
+  const double PI = std::acos(-1.0);
+  struct Rectangle final: Shape
+  {
+    Rectangle(double w, double h, p_t mid) noexcept;
+    double getArea() const noexcept override;
+    rec_t getFrameRect() const noexcept override;
+    void move(double dx, double dy) noexcept override;
+    void move(p_t point) noexcept override;
+
+  private:
+    double w_, h_;
+    p_t mid_;
+    void scale(double coef) noexcept override;
+  };
+  struct Polygon final: Shape
+  {
+    Polygon(size_t size, p_t *pts);
+    double getArea() const noexcept override;
+    rec_t getFrameRect() const noexcept override;
+    void move(double dx, double dy) noexcept override;
+    void move(p_t point) noexcept override;
+
+  private:
+    size_t size_;
+    p_t *pts_;
+    p_t mid_;
+    void scale(double coef) noexcept override;
+  };
+  struct Bubble final: Shape
+  {
+    Bubble(double r, p_t dot) noexcept;
+    double getArea() const noexcept override;
+    rec_t getFrameRect() const noexcept override;
+    void move(double dx, double dy) noexcept override;
+    void move(p_t point) noexcept override;
+
+  private:
+    double r_;
+    p_t dot_;
+    void scale(double coef) noexcept override;
+  };
+}
+#endif
